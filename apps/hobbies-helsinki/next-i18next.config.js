4404b5aa7e0b1580b0452db9437e6ebe464d73dd
Niko Lindroos <niko.j.makela@gmail.com>
2022-10-04 15:50:59 +0300

    fix: linter issues related to i18n

--- apps/hobbies-helsinki/next-i18next.config.js
+++ apps/hobbies-helsinki/next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'default',
     locales: ['default', 'fi', 'sv', 'en'],
-    localeDetection: false,
+    // localeDetection: false,
   },
   trailingSlash: true,
 };

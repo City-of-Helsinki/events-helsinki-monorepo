04ee7a58e5d2e903dd4acf31402eb15abe87f8f2
Niko Lindroos <niko.j.makela@gmail.com>
2022-10-05 16:17:33 +0300

    chore: add I18n namespace
    copy the idea from nextjs-example app

--- apps/hobbies-helsinki/src/types.d/react-i18next.d.ts
+++ apps/hobbies-helsinki/src/types.d/react-i18next.d.ts
@@ -5,11 +5,12 @@
  * @link https://react.i18next.com/latest/typescript
  */
 import 'react-i18next';
-import type { I18nNamespaces } from 'events-helsinki-common-i18n';
+import type { I18nNamespaces as CommonI18nNamespaces } from 'events-helsinki-common-i18n';
+import type { I18nNamespaces as HobbiesI18nNamespaces } from '../lib/i18n/I18nNamespaces';
 
 declare module 'react-i18next' {
   interface CustomTypeOptions {
     defaultNS: 'common';
-    resources: I18nNamespaces;
+    resources: CommonI18nNamespaces & HobbiesI18nNamespaces;
   }
 }

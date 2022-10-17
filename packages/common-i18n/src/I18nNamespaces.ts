15fe320da886915495878a7233eb4dab1809a1c6
Niko Lindroos <niko.j.makela@gmail.com>
2022-10-17 20:56:01 +0300

    feat: add new translations to common-i18n

--- packages/common-i18n/src/I18nNamespaces.ts
+++ packages/common-i18n/src/I18nNamespaces.ts
@@ -1,6 +1,11 @@
+import type cms from './locales/fi/cms.json';
 import type common from './locales/fi/common.json';
 import type demo from './locales/fi/demo.json';
+import type event from './locales/fi/event.json';
+import type footer from './locales/fi/footer.json';
 import type home from './locales/fi/home.json';
+import type notFound from './locales/fi/notFound.json';
+import type search from './locales/fi/search.json';
 import type system from './locales/fi/system.json';
 
 export type I18nNamespaces = {
@@ -8,4 +13,9 @@ export type I18nNamespaces = {
   demo: typeof demo;
   home: typeof home;
   system: typeof system;
+  cms: typeof cms;
+  footer: typeof footer;
+  event: typeof event;
+  notFound: typeof notFound;
+  search: typeof search;
 };

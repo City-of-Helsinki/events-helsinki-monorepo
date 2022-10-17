ecfd2c9f44f57defe84f952b58202753e7ee9989
Niko Lindroos <niko.j.makela@gmail.com>
2022-10-17 20:58:50 +0300

    fix: eslinter issues related to resolvers of the monorepo packages

--- packages/eslint-config-bases/.eslintrc.js
+++ packages/eslint-config-bases/.eslintrc.js
@@ -4,4 +4,17 @@ module.exports = {
   root: true,
   ignorePatterns: [...getDefaultIgnorePatterns()],
   extends: ['./src/bases/typescript', './src/bases/prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['tsconfig.json', 'package/tsconfig.json'],
+      },
+      node: {
+        project: ['tsconfig.json', 'package/tsconfig.json'],
+      },
+    },
+  },
 };

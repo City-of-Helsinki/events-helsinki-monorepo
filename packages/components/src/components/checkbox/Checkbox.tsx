9ace7bc937f59fdde651172ce7d038765d7a2fa9
Ivan Melnik <melnik.job@gmail.com>
2022-11-09 13:41:55 +0200

    feat: input components theming

--- packages/components/src/components/checkbox/Checkbox.tsx
+++ packages/components/src/components/checkbox/Checkbox.tsx
@@ -6,12 +6,17 @@ const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
   (props, ref) => {
     const customStyles = {
       '--background-unselected': 'var(--color-white)',
-      '--background-selected': 'var(--color-coat-of-arms)',
-      '--background-selected-hover': 'var(--color-coat-of-arms-dark)',
-      '--background-hover': 'var(--color-coat-of-arms-dark)',
-      '--border-color-selected': 'var(--color-coat-of-arms-dark)',
-      '--border-color-selected-focus': 'var(--color-coat-of-arms-dark)',
-      '--border-color-selected-hover': 'var(--color-coat-of-arms-dark)',
+      '--background-selected': 'var(--color-input-dark, --color-coat-of-arms)',
+      '--background-selected-hover':
+        'var(--color-input-dark, --color-coat-of-arms-dark)',
+      '--background-hover':
+        'var(--color-input-dark, --color-coat-of-arms-dark)',
+      '--border-color-selected':
+        'var(--color-input-dark,--color-coat-of-arms-dark)',
+      '--border-color-selected-focus':
+        'var(--color-input-dark, --color-coat-of-arms-dark)',
+      '--border-color-selected-hover':
+        'var(--color-input-dark,--color-coat-of-arms-dark)',
       lineHeight: '24px',
     } as React.CSSProperties;
 

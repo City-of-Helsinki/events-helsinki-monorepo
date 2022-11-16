70d1e2338c5a390c472a17486190f778a597c2ba
Jaakko Nenonen <jaakko@codelicious.fi>
2022-11-16 17:36:10 +0200

    chore: add eslint plugin for testcafe

--- packages/eslint-config-bases/src/bases/index.js
+++ packages/eslint-config-bases/src/bases/index.js
@@ -1,12 +1,11 @@
 module.exports = {
   graphqlSchema: require('./graphql-schema'),
   jest: require('./jest'),
-  playwright: require('./playwright'),
   react: require('./react'),
   regexp: require('./regexp'),
   reactTestingLibrary: require('./rtl'),
   sonar: require('./sonar'),
   storybook: require('./storybook'),
-  tailwind: require('./tailwind'),
+  testcafe: require('./testcafe'),
   typescript: require('./typescript'),
 };

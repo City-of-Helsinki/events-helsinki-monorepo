8ab29fce79d678898b0bd1614f409148f8f777bd
Niko Lindroos <niko.j.makela@gmail.com>
2022-11-29 17:54:54 +0200

    test: search page's browser test base
    HH-255

---
+++ packages/common-tests/browser-tests/page-model/eventSearchPage.ts
@@ -0,0 +1,84 @@
+import { screen } from '@testing-library/testcafe';
+import { t } from 'testcafe';
+import { initTestI18n as i18n } from '../../../common-i18n/src';
+import type { AppNamespace } from '../types/app-namespace';
+
+class EventSearchPage {
+  searchText = 'helsinki';
+  private appNamespace: 'appHobbies' | 'appEvents' | 'appSports';
+
+  constructor(appNamespace: AppNamespace) {
+    this.appNamespace = appNamespace;
+  }
+
+  protected get searchFormHeading() {
+    const searchHeadingText = i18n.t(`search:search.labelSearchField`);
+    return screen.getByRole('heading', {
+      name: searchHeadingText,
+    });
+  }
+
+  protected get searchButton() {
+    const searchButtonText = i18n.t(`search:search.buttonSearch`);
+    return screen.getByRole('button', {
+      name: searchButtonText,
+    });
+  }
+
+  protected get autoSuggestInput() {
+    const searchPlaceholderText = i18n.t(
+      `${this.appNamespace}:search.search.placeholder`
+    );
+    return screen.getByPlaceholderText(searchPlaceholderText);
+  }
+
+  protected get buttonLoadMore() {
+    const loadMoreText = i18n.t('search:buttonLoadMore');
+    return screen.queryByRole('button', {
+      name: loadMoreText,
+    });
+  }
+
+  protected get results() {
+    const cardLinkRoleName = i18n
+      .t('event:eventCard.ariaLabelLink')
+      .replace('{{name}}', '*.');
+    return screen.queryAllByRole('link', {
+      name: new RegExp(cardLinkRoleName, 'i'),
+    });
+  }
+
+  protected get notFoundResult() {
+    const notFoundText = i18n.t('search:searchNotification');
+    return screen.queryByText(notFoundText);
+  }
+
+  public async makeSuccessfulSearch() {
+    await t.typeText(this.autoSuggestInput, this.searchText);
+    await t.click(this.searchButton);
+    t.expect(this.notFoundResult.exists).notOk;
+    const results = this.results;
+    await t.expect(results.count).gte(1); // At least one result should be found
+    await t.expect(results.count).lte(10); // max 10 result per page
+  }
+
+  public async makeUnsuccessfulSearch() {
+    await t.typeText(
+      this.autoSuggestInput,
+      'thisisatextthatverylikelycannotbefound'
+    );
+    await t.click(this.searchButton);
+    t.expect(this.notFoundResult.exists).ok;
+    await t.expect(this.results.count).eql(0); // no cards returned
+  }
+
+  public async verify() {
+    // eslint-disable-next-line no-console
+    console.log('EventSearchPage: verify');
+    await t.expect(this.searchFormHeading.exists).ok();
+    await this.makeSuccessfulSearch();
+    await this.makeUnsuccessfulSearch();
+  }
+}
+
+export default EventSearchPage;
--- packages/common-tests/browser-tests/page-model/index.ts
+++ packages/common-tests/browser-tests/page-model/index.ts
@@ -1,3 +1,4 @@
 export { default as ConsentModal } from './consent-modal';
 export { default as Header } from './header';
 export { default as LandingPage } from './landingPage';
+export { default as EventSearchPage } from './eventSearchPage';

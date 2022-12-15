c4aea27d8267800602f07379be49472da046c865
Niko Lindroos <niko.j.makela@gmail.com>
2022-12-15 14:53:36 +0200

    feat(sports): service map component for events and venues
    Migrate the service map implementation into a shape
    that the same code can be used for the venues and the events

--- packages/components/src/components/domain/event/eventLocation/EventLocation.tsx
+++ packages/components/src/components/domain/event/eventLocation/EventLocation.tsx
@@ -1,71 +1,51 @@
-import { IconLocation, useCookies } from 'hds-react';
 import React from 'react';
-import { Link, SecondaryLink } from 'react-helsinki-headless-cms';
-
-import { useEventTranslation, useLocale } from '../../../../hooks';
-import type { EventFields } from '../../../../types';
+import { MapBox } from '../../../../components';
+import { useCommonTranslation, useLocale } from '../../../../hooks';
+import type { AppLanguage, EventFields } from '../../../../types';
 import { getEventFields, getServiceMapUrl } from '../../../../utils';
-import styles from './eventLocation.module.scss';
-import LocationText from './EventLocationText';
 
 interface Props {
   event: EventFields;
 }
 
+export const getLocationStr = (
+  event: EventFields,
+  locale: AppLanguage,
+  showDistrict: boolean,
+  showLocationName: boolean
+) => {
+  const { addressLocality, district, locationName, streetAddress } =
+    getEventFields(event, locale);
+
+  return [
+    showLocationName ? locationName : null,
+    streetAddress,
+    showDistrict ? district : null,
+    addressLocality,
+  ]
+    .filter((e) => e)
+    .join(', ');
+};
+
 const EventLocation: React.FC<Props> = ({ event }) => {
-  const { t } = useEventTranslation();
+  const { t } = useCommonTranslation();
   const locale = useLocale();
   const { googleDirectionsLink, hslDirectionsLink, name } = getEventFields(
     event,
     locale
   );
-  const { getAllConsents } = useCookies();
-  const getConsentStatus = (cookieId: string) => {
-    const consents = getAllConsents();
-    return consents[cookieId];
-  };
-  const isServiceMapEnabled = getConsentStatus('servicemap_session');
 
   // TODO: Migrate the map of VenueLocation and EventLocation to a common component
   return (
-    <div className={styles.eventLocationContainer}>
-      <div className={styles.titleWrapper}>
-        <div className={styles.title}>
-          <IconLocation aria-hidden />
-          <h2>{t('event:location.title')}</h2>
-        </div>
-        <Link
-          className={styles.mapLink}
-          href={getServiceMapUrl(event, locale, false)}
-        >
-          {t('event:location.openMap')}
-        </Link>
-      </div>
-      {isServiceMapEnabled && (
-        <iframe
-          title={t('event:location.mapTitle')}
-          className={styles.mapContainer}
-          src={getServiceMapUrl(event, locale, true)}
-        ></iframe>
-      )}
-      <div className={styles.eventName}>{name}</div>
-      <div className={styles.location}>
-        <LocationText
-          event={event}
-          showDistrict={true}
-          showLocationName={false}
-        />
-      </div>
-      <SecondaryLink
-        className={styles.externalLink}
-        href={googleDirectionsLink}
-      >
-        {t('event:location.directionsGoogle')}
-      </SecondaryLink>
-      <SecondaryLink className={styles.externalLink} href={hslDirectionsLink}>
-        {t('event:location.directionsHSL')}
-      </SecondaryLink>
-    </div>
+    <MapBox
+      title={t('common:mapBox.title')}
+      serviceMapUrl={getServiceMapUrl(event, locale, true)}
+      openMapUrl={getServiceMapUrl(event, locale, false)}
+      placeName={name ?? ''}
+      placeAddress={getLocationStr(event, locale, true, false)}
+      googleDirectionsLink={googleDirectionsLink}
+      hslDirectionsLink={hslDirectionsLink}
+    />
   );
 };
 

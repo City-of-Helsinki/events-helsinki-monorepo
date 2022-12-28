6dba429f3b7f30edc17c8ed45f4871d29bfeaf00
Jaakko Nenonen <jaakko@codelicious.fi>
2022-12-28 22:44:38 +0200

    feat(liikunta-347): remove wagtail cms code

--- proxies/events-graphql-proxy/src/types.ts
+++ proxies/events-graphql-proxy/src/types.ts
@@ -1,21 +1,13 @@
-import type AboutPageAPI from './datasources/aboutPage';
-import type AccessibilityPageAPI from './datasources/accessibilityPage';
-import type CollectionAPI from './datasources/collection';
 import type EventAPI from './datasources/event';
 import type KeywordAPI from './datasources/keyword';
-import type LandingPageAPI from './datasources/landingPage';
 import type NeighborhoodAPI from './datasources/neighborhood';
 import type OrganizationAPI from './datasources/organization';
 import type PlaceAPI from './datasources/place';
 import type { QueryResolvers as GeneratedQueryResolvers } from './types/types';
 
 export type DataSources = {
-  aboutPageAPI: AboutPageAPI;
-  accessibilityPageAPI: AccessibilityPageAPI;
-  collectionAPI: CollectionAPI;
   eventAPI: EventAPI;
   keywordAPI: KeywordAPI;
-  landingPageAPI: LandingPageAPI;
   neighborhoodAPI: NeighborhoodAPI;
   organizationAPI: OrganizationAPI;
   placeAPI: PlaceAPI;

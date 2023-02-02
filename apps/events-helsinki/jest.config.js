268dd93c6296d68be0fb8ccf866654a86b89758c
Jaakko Nenonen <jaakko@codelicious.fi>
2023-02-02 22:59:14 +0200

    perf(liikunta-389): try swc/jest

--- apps/events-helsinki/jest.config.js
+++ apps/events-helsinki/jest.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-const { defaults: tsjPreset } = require('ts-jest/presets');
 const { pathsToModuleNameMapper } = require('ts-jest');
 
 const { getJestCachePath } = require('../../cache.config');
@@ -24,9 +22,20 @@ const config = {
   testEnvironment: 'jsdom',
   verbose: true,
   rootDir: './src',
-  // @ts-ignore
   transform: {
-    ...tsjPreset.transform,
+    '.*\\.(tsx?)$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2021',
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
   },
   setupFilesAfterEnv: ['<rootDir>/../.jest/setupTests.ts'],
   testMatch: ['<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}'],
@@ -44,12 +53,6 @@ const config = {
   coverageDirectory: '<rootDir>/../coverage',
   collectCoverageFrom: ['<rootDir>/**/*.{ts,tsx,js,jsx}', '!**/*.test.ts'],
   coveragePathIgnorePatterns: ['<rootDir>/../config/', '<rootDir>/../.jest/'],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-      tsconfig: './tsconfig.jest.json',
-    },
-  },
 };
 
 module.exports = config;

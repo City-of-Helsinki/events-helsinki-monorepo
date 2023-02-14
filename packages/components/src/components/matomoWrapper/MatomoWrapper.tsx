6528f36267e06b66c7b0ebb01d67b6bd351c5477
Ivan Melnik <melnik.job@gmail.com>
2023-02-14 11:23:00 +0200

    feat: consent extended

--- packages/components/src/components/matomoWrapper/MatomoWrapper.tsx
+++ packages/components/src/components/matomoWrapper/MatomoWrapper.tsx
@@ -1,4 +1,5 @@
 import { useMatomo } from '@jonkoops/matomo-tracker-react';
+import { useCookies } from 'hds-react';
 import { useRouter } from 'next/router';
 import type { ReactNode } from 'react';
 import React, { useEffect } from 'react';
@@ -8,15 +9,29 @@ interface Props {
 }
 
 const MatomoWrapper: React.FC<Props> = ({ children }) => {
-  const { trackPageView } = useMatomo();
+  const { trackPageView, pushInstruction } = useMatomo();
   const { asPath: pathname } = useRouter();
+  const { getAllConsents } = useCookies();
 
   // Track page changes when pathnname changes
   useEffect(() => {
+    const getConsentStatus = (cookieId: string) => {
+      const consents = getAllConsents();
+      return consents[cookieId];
+    };
+
+    // if enabled, should be callled before each trackPage instruction
+
+    if (getConsentStatus('matomo')) {
+      pushInstruction('requireCookieConsent');
+    } else {
+      pushInstruction('setCookieConsentGiven');
+    }
+
     trackPageView({
       href: window.location.href,
     });
-  }, [pathname, trackPageView]);
+  }, [getAllConsents, pathname, pushInstruction, trackPageView]);
 
   return <>{children}</>;
 };

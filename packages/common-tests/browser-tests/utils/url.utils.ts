457619823f952d3e9876225cd270296ae593a5fa
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-02-16 14:23:34 +0200

    Browser test url fix whenends to '/'

--- packages/common-tests/browser-tests/utils/url.utils.ts
+++ packages/common-tests/browser-tests/utils/url.utils.ts
@@ -1,10 +1,10 @@
 const localEnvUrl = 'http://localhost:3000';
 
 export const getEnvUrl = (path = ''): string => {
-  const baseUrl = process.env.BROWSER_TESTS_ENV_URL ?? localEnvUrl;
-  return `${baseUrl.replace(/,+$/, '')}${
-    path?.startsWith('/') ? path : `/${path ?? ''}`
-  }`;
+  let baseUrl = process.env.BROWSER_TESTS_ENV_URL ?? localEnvUrl;
+  baseUrl = `${baseUrl?.endsWith('/') ? baseUrl.slice(0, -1) : `${baseUrl}`}`;
+
+  return `${baseUrl}${path?.startsWith('/') ? path : `/${path ?? ''}`}`;
 };
 
 // eslint-disable-next-line no-console

73fb12be2de7464840b171528ae09f970bebed06
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-02-16 22:42:55 +0200

    userole inside test

--- apps/sports-helsinki/browser-tests/landing-page/landing-page.testcafe.ts
+++ apps/sports-helsinki/browser-tests/landing-page/landing-page.testcafe.ts
@@ -4,10 +4,11 @@ import {
   allCookiesUser,
 } from 'events-helsinki-common-tests/browser-tests';
 
-fixture('Landing page header').beforeEach(async (t) => {
+fixture.disablePageCaching('Landing page header').beforeEach(async (t) => {
   await t.useRole(allCookiesUser).navigateTo(getEnvUrl());
 });
 
-test('Verify header title', async () => {
+test('Verify header title', async (t) => {
+  await t.useRole(allCookiesUser).navigateTo(getEnvUrl());
   await changeLanguageAndTrySearch('appSports');
 });

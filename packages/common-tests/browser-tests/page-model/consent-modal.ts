a673e4a12a97af0c37aa664a8c245bdc9d747644
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-02-21 07:53:14 +0200

    Wait cookie 30s

--- packages/common-tests/browser-tests/page-model/consent-modal.ts
+++ packages/common-tests/browser-tests/page-model/consent-modal.ts
@@ -39,7 +39,7 @@ class ConsentModal {
       await t.wait(100);
       i++;
       cookies = await t.getCookies(this.consentAllCookieName);
-    } while (i < 100);
+    } while (i < 300);
 
     console.log('Cookie wait time ' + i / 10 + 's');
   }

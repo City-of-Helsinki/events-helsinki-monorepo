cd590f96167c7e51f60ab58709b5284ed2347ab4
Ivan Melnik <melnik.job@gmail.com>
2023-02-21 08:38:26 +0200

    fix: refactor main tags and article styles

--- apps/events-helsinki/src/pages/articles/[...slug].tsx
+++ apps/events-helsinki/src/pages/articles/[...slug].tsx
@@ -56,6 +56,7 @@ const NextCmsArticle: NextPage<{
   return (
     <MatomoWrapper>
       <RHHCPage
+        className="article-page"
         navigation={<Navigation page={article} />}
         content={
           <RHHCPageContent

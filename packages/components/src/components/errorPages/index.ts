9bacbc861fe9e599359f757c184ddd6ebe107775
Jaakko Nenonen <jaakko@codelicious.fi>
2023-03-03 13:57:54 +0200

    refactor: simplify root apps

--- packages/components/src/components/errorPages/index.ts
+++ packages/components/src/components/errorPages/index.ts
@@ -1,3 +1,4 @@
+export { default as ErrorFallback } from './ErrorFallback';
 export { default as ErrorPage } from './ErrorPage';
 export { default as NotFound } from './NotFound';
 export { default as UnknownError } from './UnknownError';

23e524f570ebfe12c9128b0521f11459f102420e
Niko Lindroos <niko.j.makela@gmail.com>
2023-03-06 12:35:41 +0200

    refactor: use redirects instead of rewrites when processing old URLs
    TH-1302

--- apps/events-helsinki/next.config.js
+++ apps/events-helsinki/next.config.js
@@ -1,6 +1,22 @@
 const packageJson = require('./package.json');
 const i18nRoutes = require('./i18nRoutes.config');
+const redirectRoutes = require('./redirectRoutes.config');
 const { i18n } = require('./next-i18next.config');
 const nextBaseConfig = require('../../next.base.config');
-
-module.exports = nextBaseConfig({ packageJson, i18nRoutes, i18n });
+module.exports = nextBaseConfig({
+  packageJson,
+  i18nRoutes,
+  i18n,
+  override: {
+    async redirects() {
+      return Object.entries(redirectRoutes).flatMap(([destination, sources]) =>
+        sources.map(({ source, locale }) => ({
+          destination,
+          source: `/${locale}${source}`,
+          locale: false,
+          permanent: true,
+        }))
+      );
+    },
+  },
+});

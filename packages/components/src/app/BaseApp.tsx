b7e1754b29d52b0b596e05b1a966b59b990c7587
Niko Lindroos <niko.j.makela@gmail.com>
2023-03-06 17:44:37 +0200

    chore: remove the needless React Suspense container
    HH-254.
    
    The React Suspense is still an experimental feature,
    that is not supported by our current NextJS version,
    since it does not work properly in SSR.
    
    There will be better support in the NextJS 13, but that needs more refactoring.
    More details: https://beta.nextjs.org/docs/data-fetching/streaming-and-suspense
    and https://beta.reactjs.org/reference/react/Suspense.
    It should be noted that also the used middleware processor matters.

--- packages/components/src/app/BaseApp.tsx
+++ packages/components/src/app/BaseApp.tsx
@@ -4,7 +4,6 @@ import {
 } from '@jonkoops/matomo-tracker-react';
 import 'nprogress/nprogress.css';
 
-import { LoadingSpinner } from 'hds-react';
 import type { SSRConfig } from 'next-i18next';
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
@@ -16,28 +15,9 @@ import ErrorFallback from '../components/errorPages/ErrorFallback';
 import EventsCookieConsent from '../components/eventsCookieConsent/EventsCookieConsent';
 import ResetFocus from '../components/resetFocus/ResetFocus';
 import { GeolocationProvider } from '../geolocation';
-import useCommonTranslation from '../hooks/useCommonTranslation';
 import { NavigationProvider } from '../navigationProvider';
 import type { NavigationProviderProps } from '../navigationProvider';
 import type { CmsRoutedAppHelper, HeadlessCMSHelper } from '../utils';
-
-function PageLoadingSpinner() {
-  return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <LoadingSpinner />
-    </div>
-  );
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 
 export type Props = {
   children: React.ReactNode;
@@ -58,7 +38,6 @@ function BaseApp({
   routerHelper,
   matomoConfiguration,
 }: Props) {
-  const { t } = useCommonTranslation();
   // Unset hidden visibility that was applied to hide the first server render
   // that does not include styles from HDS. HDS applies styling by injecting
   // style tags into the head. This requires the existence of a document object.
@@ -77,6 +56,7 @@ function BaseApp({
 
   const matomoInstance = React.useMemo(
     () => createMatomoInstance(matomoConfiguration),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [createMatomoInstance]
   );
 
@@ -87,25 +67,23 @@ function BaseApp({
   return (
     <CmsHelperProvider cmsHelper={cmsHelper} routerHelper={routerHelper}>
       <ErrorBoundary FallbackComponent={FallbackComponent}>
-        <React.Suspense fallback={<PageLoadingSpinner />}>
-          <MatomoProvider value={matomoInstance}>
-            <GeolocationProvider>
-              <NavigationProvider
-                headerMenu={headerMenu}
-                footerMenu={footerMenu}
-                languages={languages}
-              >
-                <ResetFocus />
-                {children}
-                <EventsCookieConsent
-                  allowLanguageSwitch={false}
-                  appName={appName}
-                />
-                <ToastContainer />
-              </NavigationProvider>
-            </GeolocationProvider>
-          </MatomoProvider>
-        </React.Suspense>
+        <MatomoProvider value={matomoInstance}>
+          <GeolocationProvider>
+            <NavigationProvider
+              headerMenu={headerMenu}
+              footerMenu={footerMenu}
+              languages={languages}
+            >
+              <ResetFocus />
+              {children}
+              <EventsCookieConsent
+                allowLanguageSwitch={false}
+                appName={appName}
+              />
+              <ToastContainer />
+            </NavigationProvider>
+          </GeolocationProvider>
+        </MatomoProvider>
       </ErrorBoundary>
     </CmsHelperProvider>
   );

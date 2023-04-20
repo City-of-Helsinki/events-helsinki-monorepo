ac476b95fd00a4f3d4f0524eb10ba83d635f1c89
Pekka Kela <pekka.kela@vincit.fi>
2023-04-20 09:15:49 +0300

    fix: delete button for show results in finnish

--- apps/events-helsinki/src/domain/search/eventSearch/searchResultList/__tests__/ResultsInfo.test.tsx
+++ apps/events-helsinki/src/domain/search/eventSearch/searchResultList/__tests__/ResultsInfo.test.tsx
@@ -1,12 +1,7 @@
-import type { AppLanguage } from '@events-helsinki/components';
-import useLocale from '@events-helsinki/components/hooks/useLocale';
-
 import * as React from 'react';
 
-import { render, screen, userEvent, waitFor } from '@/test-utils';
+import { render, screen } from '@/test-utils';
 import ResultsInfo from '../ResultsInfo';
-
-const Locale = { useLocale };
 
 it('events with 0 results matches snapshot for no results', () => {
   const { container } = render(<ResultsInfo resultsCount={0} />);
@@ -38,36 +33,3 @@ it.each([1, 4])(
     });
   }
 );
-
-it.skip.each<[AppLanguage, number]>([
-  ['en', 4],
-  ['en', 0],
-  ['sv', 0],
-])(
-  'renders language change button under search results when current language is %s and there are %i %s search items',
-  async (language, resultsCount) => {
-    jest.spyOn(Locale, 'useLocale').mockReturnValue('fi');
-
-    const { router } = render(<ResultsInfo resultsCount={resultsCount} />);
-
-    await userEvent.click(
-      screen.getByRole('button', { name: 'Näytä hakutulokset suomeksi' })
-    );
-
-    await waitFor(() => {
-      expect(router.pathname).toBe(`/haku`);
-    });
-  }
-);
-
-// eslint-disable-next-line max-len
-it.skip('renders does not render language change button under eventss search results when current language is Finnish', () => {
-  // @ts-ignore
-  jest.spyOn(Locale, 'useLocale').mockReturnValue('fi');
-
-  render(<ResultsInfo resultsCount={0} />);
-
-  expect(
-    screen.queryByRole('button', { name: 'Näytä hakutulokset suomeksi' })
-  ).not.toBeInTheDocument();
-});
--- apps/sports-helsinki/src/domain/search/eventSearch/searchResultList/ResultsInfo.tsx
+++ apps/sports-helsinki/src/domain/search/eventSearch/searchResultList/ResultsInfo.tsx
@@ -1,52 +1,21 @@
-import { useLocale } from '@events-helsinki/components';
-import type { AppLanguage } from '@events-helsinki/components';
-import { Button, IconSearch } from 'hds-react';
-import { useRouter } from 'next/router';
+import { IconSearch } from 'hds-react';
+
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 
-import { ROUTES } from '../../../../constants';
-import routerHelper from '../../../../domain/app/routerHelper';
 import styles from './resultsInfo.module.scss';
 
 const ResultsInfoContainer: React.FC<{
   resultsCount: number;
 }> = ({ resultsCount }) => {
   const { t } = useTranslation('search');
-  const router = useRouter();
-  const locale = useLocale();
-  const isFinnish = locale === 'fi';
-
-  const goToFinnishSearch = () => {
-    router.push(routerHelper.getI18nPath(ROUTES.SEARCH, 'fi' as AppLanguage));
-  };
-
-  const ActionButtons = () => (
-    <>
-      {!isFinnish && (
-        <Button variant="success" onClick={goToFinnishSearch}>
-          {t('searchNotification.buttons.labelSearchInFinnish')}
-        </Button>
-      )}
-    </>
-  );
 
   if (resultsCount === 0) {
-    return (
-      <ResultsInfo
-        bigText={t(`searchNotification.noResultsTitle`)}
-        actionsSection={<ActionButtons />}
-      />
-    );
+    return <ResultsInfo bigText={t(`searchNotification.noResultsTitle`)} />;
   }
 
   if (resultsCount < 5) {
-    return (
-      <ResultsInfo
-        bigText={t(`searchNotification.fewResultsTitle`)}
-        actionsSection={<ActionButtons />}
-      />
-    );
+    return <ResultsInfo bigText={t(`searchNotification.fewResultsTitle`)} />;
   }
 
   return null;
@@ -55,8 +24,7 @@ const ResultsInfoContainer: React.FC<{
 const ResultsInfo: React.FC<{
   smallText?: string;
   bigText: string;
-  actionsSection?: JSX.Element;
-}> = ({ bigText, smallText, actionsSection }) => {
+}> = ({ bigText, smallText }) => {
   return (
     <div className={styles.noResultsInfo}>
       <div className={styles.iconWrapper}>
@@ -64,7 +32,6 @@ const ResultsInfo: React.FC<{
       </div>
       <div className={styles.bigText}>{bigText}</div>
       {smallText && <div className={styles.smallText}>{smallText}</div>}
-      {actionsSection && <div className={styles.actions}>{actionsSection}</div>}
     </div>
   );
 };

a33da7bcd501ecd54ae9e5d59f91dc60d703875e
Ivan Melnik <melnik.job@gmail.com>
2023-05-04 07:47:19 +0300

    Liikunta 485 venue translations (#313)

--- apps/sports-helsinki/src/pages/search/index.tsx
+++ apps/sports-helsinki/src/pages/search/index.tsx
@@ -42,7 +42,7 @@ export async function getStaticProps(context: GetStaticPropsContext) {
         ...(await serverSideTranslationsWithCommon(language, [
           'event',
           'search',
-          'errors',
+          'venue',
         ])),
       },
     };

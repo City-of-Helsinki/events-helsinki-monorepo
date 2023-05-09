b8d9cf82f04a5060d41a8751f39ec21d9a67f746
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-05-09 13:54:37 +0300

    build: copy .next before node start

--- run_node.sh
+++ run_node.sh
@@ -1,6 +1,11 @@
 
 PROJECT=events-helsinki
 cd /app
-[ "$(ls -A /app/apps/${PROJECT}/.next/)" ] || cp -r /app/.next_orig/* /app/apps/${PROJECT}/.next/
+
+if [[ -z "$(ls -A /app/apps/${PROJECT}/.next/)" || -n "$(diff -q /app/apps/${PROJECT}/.next/BUILD_ID /app/.next_orig/BUILD_ID)" ]]; then
+    echo "Copy generated static files from .next_orig to .next"
+    rm -fr .next/*
+    cp -r -f /app/.next_orig/* /app/apps/${PROJECT}/.next/
+fi
 
 node ./apps/${PROJECT}/server.js

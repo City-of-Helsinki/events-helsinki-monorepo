9bd68e6cf135b1e06f6645afc7858c0c19e3b251
Niko Lindroos <niko.j.makela@gmail.com>
2023-05-17 18:38:29 +0300

    refactor(search): add a new controller hook for combined search
    LIIKUNTA-425.
    
    Remove the router usage totally from the
    CombinedSearchFormAdapter, since it should
    only map the URL search params to
    search form input fields.
    
    Add the removed features and tests to a new
    React-hook that acts as a controller, that
    decides when to navigate and to where.

---
+++ apps/sports-helsinki/src/domain/search/combinedSearch/hooks/__tests__/useCombinedSearchController.test.tsx
@@ -0,0 +1,87 @@
+import * as nextNavigation from 'next/navigation';
+import mockRouter from 'next-router-mock';
+import qs from 'query-string';
+import React from 'react';
+import { render } from '@/test-utils';
+import type { CombinedSearchAdapterInput } from '../../types';
+import { useCombinedSearchController } from '../useCombinedSearchController';
+
+const useSearchParamsSpy = jest.spyOn(nextNavigation, 'useSearchParams');
+
+const outputQuery: CombinedSearchAdapterInput = {
+  text: 'test text',
+  venueOrderBy: 'venue-field-asc',
+  eventOrderBy: 'event-field-asc',
+  courseOrderBy: 'course-field-asc',
+  sportsCategories: [],
+  keywords: [],
+};
+const extraParamsInOutputQuery = {
+  searchType: 'Venue',
+  unrelatedParam: 'whatever',
+};
+const inputSearchParams = qs.stringify({
+  ...outputQuery,
+  ...extraParamsInOutputQuery,
+});
+const input = new URLSearchParams(inputSearchParams);
+
+/**
+ * The purpose of this component is to call
+ * the updateRouteToSearchPage of the useCombinedSearchController.
+ * */
+function UpdateRouteToSearchPageComponent() {
+  const { updateRouteToSearchPage } = useCombinedSearchController();
+  React.useEffect(() => {
+    updateRouteToSearchPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div>UpdateRouteToSearchPageComponent</div>;
+}
+
+describe('updateRouteToSearchPage', () => {
+  it.each([
+    // Test the happy path where nothing changes
+    [input, inputSearchParams],
+    [
+      new URLSearchParams(
+        qs.stringify({
+          text: 'to be transformed to text',
+          venueOrderBy: 'to-be-order-by',
+        })
+      ),
+      qs.stringify({
+        text: 'to be transformed to text',
+        venueOrderBy: 'to-be-order-by',
+      }),
+    ],
+    // Test the backward compatibility
+    [
+      new URLSearchParams(
+        qs.stringify({
+          q: 'to be transformed to text',
+          sort: 'to-be-order-by',
+        })
+      ),
+      qs.stringify({
+        q: 'to be transformed to text',
+        sort: 'to-be-order-by',
+        text: 'to be transformed to text',
+        venueOrderBy: 'to-be-order-by',
+        eventOrderBy: 'to-be-order-by',
+        courseOrderBy: 'to-be-order-by',
+      }),
+    ],
+  ])('pushes the user to a desired url', (searchParams, outputSearchParams) => {
+    mockRouter.setCurrentUrl(`/?${searchParams.toString()}`);
+    // TODO: Remove the useSearchParamsSpy when mockRouter supports next/navigation.
+    useSearchParamsSpy.mockImplementation(
+      () =>
+        new nextNavigation.ReadonlyURLSearchParams(
+          new URLSearchParams(searchParams.toString())
+        )
+    );
+    render(<UpdateRouteToSearchPageComponent />);
+    expect(qs.stringify(mockRouter.query)).toBe(outputSearchParams);
+  });
+});

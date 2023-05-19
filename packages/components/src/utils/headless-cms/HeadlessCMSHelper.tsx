41d9715992cf65aab8d4fa300df6ace255742248
Ivan Melnik <melnik.job@gmail.com>
2023-05-19 11:11:17 +0300

    fix: decode of show all url (#329)

--- packages/components/src/utils/headless-cms/HeadlessCMSHelper.tsx
+++ packages/components/src/utils/headless-cms/HeadlessCMSHelper.tsx
@@ -1,4 +1,5 @@
 import format from 'date-fns/format';
+import parse from 'html-react-parser';
 import React from 'react';
 import type {
   ArticleType,
@@ -180,7 +181,7 @@ export class HeadlessCMSHelper {
           <EventSearchCollection
             {...commonCollectionProps}
             collection={collection}
-            showAllUrl={collection.showAllUrl}
+            showAllUrl={parse(collection.showAllUrl || '').toString()}
           />
         );
       } else if (isEventSelectionCollection(collection)) {

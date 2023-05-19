12332cd5be5da2bd1aa95de85d4cd5f8230444c7
Niko Lindroos <niko.j.makela@gmail.com>
2023-05-19 15:02:22 +0300

    fix(search): map query should use the context search variables
    LIIKUNTA-425

--- apps/sports-helsinki/src/domain/unifiedSearch/useUnifiedSearchMapQuery.ts
+++ apps/sports-helsinki/src/domain/unifiedSearch/useUnifiedSearchMapQuery.ts
@@ -1,14 +1,18 @@
 import type { SearchMapQueryVariables } from '@events-helsinki/components';
 import { useSearchMapQuery } from '@events-helsinki/components';
-import type { OverridableVariables } from './useUnifiedSearchVariables';
-import useUnifiedSearchVariables from './useUnifiedSearchVariables';
+import { useCombinedSearchContext } from '../search/combinedSearch/adapters/CombinedSearchContext';
+
+export type OverridableVariables = {
+  first: number;
+};
 
 export default function useUnifiedSearchMapQuery(
   variables: OverridableVariables
 ) {
+  const { searchVariables } = useCombinedSearchContext();
   const { fetchMore, ...delegated } = useSearchMapQuery({
     ssr: false,
-    variables: useUnifiedSearchVariables(variables),
+    variables: { ...searchVariables.venue, ...variables },
   });
 
   const handleFetchMore = (variables: Partial<SearchMapQueryVariables>) =>

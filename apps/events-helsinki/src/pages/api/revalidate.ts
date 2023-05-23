72e7acc84d20b6805b605f96af4024b5f045624f
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-05-23 10:08:42 +0300

    chore: add logging for revalidation, optional uri for revalidate one page

--- apps/events-helsinki/src/pages/api/revalidate.ts
+++ apps/events-helsinki/src/pages/api/revalidate.ts
@@ -12,29 +12,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  let customUri = '';
+
   // Check for secret to confirm this is a valid request
   if (req.body.secret !== process.env.REVALIDATE_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
   }
+  if (req.body.uri) {
+    customUri = req.body.uri;
+  }
 
   try {
-    // main pages
-    for (const page of APP_LANGUAGES) {
-      _revalidate(res, '/' + page);
-    }
+    if (customUri === '') {
+      // main pages
+      for (const page of APP_LANGUAGES) {
+        _revalidate(res, '/' + page);
+      }
 
-    // get all articles
-    const articlePageInfos = await getAllArticles(eventsApolloClient);
-    for (const pageInfo of articlePageInfos) {
-      _revalidate(res, pageInfo.uri);
-    }
+      // get all articles
+      const articlePageInfos = await getAllArticles(eventsApolloClient);
+      for (const pageInfo of articlePageInfos) {
+        _revalidate(res, pageInfo.uri);
+      }
 
-    // get all pages
-    const pagePageInfos = await getAllPages(eventsApolloClient);
-    for (const pageInfo of pagePageInfos) {
-      _revalidate(res, pageInfo.uri);
+      // get all pages
+      const pagePageInfos = await getAllPages(eventsApolloClient);
+      for (const pageInfo of pagePageInfos) {
+        _revalidate(res, pageInfo.uri);
+      }
+    } else {
+      _revalidate(res, customUri);
     }
-
     return res.status(200).json({ revalidatetrigger: true });
   } catch (err) {
     // If there was an error, Next.js will continue
@@ -50,6 +58,7 @@ async function _revalidate(res: NextApiResponse, uri: string) {
     const _uri = uri.replace(/\/$/, '');
     if (_uri.length < 1) return null;
 
+    staticGenerationLogger.info(`Revalidate a page: ${uri}`);
     await res.revalidate(_uri);
   } catch (err) {
     staticGenerationLogger.error(`Error while revalidate a page: ${uri}`, err);

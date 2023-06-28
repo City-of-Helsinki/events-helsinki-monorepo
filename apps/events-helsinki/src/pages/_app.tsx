321e272230b18b84ce6a08c495f462a0bd197fd6
Niko Lindroos <niko.j.makela@gmail.com>
2023-06-28 17:37:23 +0300

    refactor: error boundary usage
    HH-309.
    
    The Error boundary usage needs a provider after the react-error-boundary was updated to the latest.

--- apps/events-helsinki/src/pages/_app.tsx
+++ apps/events-helsinki/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import {
   BaseApp,
   useCommonTranslation,
 } from '@events-helsinki/components';
+import { FallbackComponent } from '@events-helsinki/components/app/BaseApp';
 import type { AppProps as NextAppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import type { SSRConfig } from 'next-i18next';
@@ -12,6 +13,7 @@ import { appWithTranslation } from 'next-i18next';
 import React from 'react';
 
 import '../styles/globals.scss';
+import { ErrorBoundary } from 'react-error-boundary';
 import nextI18nextConfig from '../../next-i18next.config';
 import { ROUTES } from '../constants';
 import AppConfig from '../domain/app/AppConfig';
@@ -30,24 +32,30 @@ function MyApp({ Component, pageProps }: AppProps<CustomPageProps>) {
   const { t } = useCommonTranslation();
   const locale = useLocale();
   const { asPath, pathname } = useRouter();
-
+  const appName = t('appEvents:appName');
   return (
-    <EventsApolloProvider>
-      <BaseApp
-        appName={t('appEvents:appName')}
-        cmsHelper={cmsHelper}
-        routerHelper={routerHelper}
-        matomoConfiguration={AppConfig.matomoConfiguration}
-        askemFeedbackConfiguration={AppConfig.askemFeedbackConfiguration(
-          locale
-        )}
-        withConsent={pathname !== ROUTES.COOKIE_CONSENT}
-        asPath={asPath}
-        {...pageProps}
-      >
-        <Component {...pageProps} />
-      </BaseApp>
-    </EventsApolloProvider>
+    <ErrorBoundary
+      FallbackComponent={({ error }) => (
+        <FallbackComponent error={error} appName={appName} />
+      )}
+    >
+      <EventsApolloProvider>
+        <BaseApp
+          appName={appName}
+          cmsHelper={cmsHelper}
+          routerHelper={routerHelper}
+          matomoConfiguration={AppConfig.matomoConfiguration}
+          askemFeedbackConfiguration={AppConfig.askemFeedbackConfiguration(
+            locale
+          )}
+          withConsent={pathname !== ROUTES.COOKIE_CONSENT}
+          asPath={asPath}
+          {...pageProps}
+        >
+          <Component {...pageProps} />
+        </BaseApp>
+      </EventsApolloProvider>
+    </ErrorBoundary>
   );
 }
 

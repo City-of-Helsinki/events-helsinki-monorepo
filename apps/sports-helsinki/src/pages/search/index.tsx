77b9737e76dd076c8c04a76b14b5bce7ea62fe75
Ivan Melnik <melnik.job@gmail.com>
2023-07-05 09:44:48 +0300

    fix: remove unused code

--- apps/sports-helsinki/src/pages/search/index.tsx
+++ apps/sports-helsinki/src/pages/search/index.tsx
@@ -7,26 +7,15 @@ import {
   getLanguageOrDefault,
   usePageScrollRestoration,
 } from '@events-helsinki/components';
-import type { GetStaticPropsContext, NextPage } from 'next';
+import type { GetStaticPropsContext } from 'next';
 import React, { useContext } from 'react';
-import type { PageType } from 'react-helsinki-headless-cms';
-import type {
-  PageQuery,
-  PageQueryVariables,
-} from 'react-helsinki-headless-cms/apollo';
-import {
-  PageDocument,
-  Page as HCRCApolloPage,
-} from 'react-helsinki-headless-cms/apollo';
+import { Page as HCRCApolloPage } from 'react-helsinki-headless-cms/apollo';
 import { ROUTES } from '../../constants';
 import getSportsStaticProps from '../../domain/app/getSportsStaticProps';
-import { sportsApolloClient } from '../../domain/clients/sportsApolloClient';
 import serverSideTranslationsWithCommon from '../../domain/i18n/serverSideTranslationsWithCommon';
 import CombinedSearchPage from '../../domain/search/combinedSearch/CombinedSearchPage';
 
-const Search: NextPage<{
-  page: PageType;
-}> = ({ page }) => {
+export default function Search() {
   const { footerMenu } = useContext(NavigationContext);
   const { t } = useCommonTranslation();
   usePageScrollRestoration();
@@ -47,18 +36,10 @@ const Search: NextPage<{
       />
     </MatomoWrapper>
   );
-};
-
-export default Search;
+}
 
 export async function getStaticProps(context: GetStaticPropsContext) {
   return getSportsStaticProps(context, async () => {
-    const { currentPage: page } = await getProps();
-    if (!page) {
-      return {
-        notFound: true,
-      };
-    }
     const language = getLanguageOrDefault(context.locale);
     return {
       props: {
@@ -67,30 +48,7 @@ export async function getStaticProps(context: GetStaticPropsContext) {
           'search',
           'venue',
         ])),
-        page,
       },
     };
   });
 }
-
-const getProps = async () => {
-  const { data: pageData } = await sportsApolloClient.query<
-    PageQuery,
-    PageQueryVariables
-  >({
-    query: PageDocument,
-    variables: {
-      id: 'search',
-    },
-    fetchPolicy: 'no-cache', // FIXME: network-only should work better, but for some reason it only updates once.
-  });
-
-  const currentPage = pageData.page;
-  // TODO: Breadcrumbs are unstyled, so left disabled
-  // const breadcrumbs = _getBreadcrumbs(
-  //   apolloClient,
-  //   (context.params?.slug ?? []) as string[]
-  // );
-
-  return { currentPage, breadcrumbs: [], apolloClient: sportsApolloClient };
-};

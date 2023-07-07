77c5c838ece7fc521218a406a054ea5f2562ccda
Ivan Melnik <melnik.job@gmail.com>
2023-07-07 11:36:25 +0300

    fix: venue page metadata (#374)

---
+++ apps/sports-helsinki/src/domain/venue/venuePageMeta/VenuePageMeta.tsx
@@ -0,0 +1,46 @@
+import type { Venue } from '@events-helsinki/components';
+import Head from 'next/head';
+import React from 'react';
+import { useConfig } from 'react-helsinki-headless-cms';
+
+interface Props {
+  venue: Venue;
+}
+
+const VenuePageMeta: React.FC<Props> = ({ venue }) => {
+  const { meta } = useConfig();
+
+  const title = venue.name || '';
+  const description = venue.description || '';
+  const image = venue.image || '';
+
+  const openGraphProperties: { [key: string]: string } = {
+    description: description,
+    image: image,
+    title: title,
+  };
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="twitter:card" content="summary" />
+      {Object.entries(openGraphProperties).map(([property, value]) => (
+        <meta key={property} property={`og:${property}`} content={value} />
+      ))}
+      <meta
+        name="keywords"
+        content={venue.ontologyWords
+          .map((keyword) => keyword?.label?.toLowerCase())
+          .filter((k) => k)
+          .join(', ')}
+      />
+      <link rel="icon" href={meta?.favIconUrl} sizes="any" />
+      <link rel="apple-touch-icon" href={meta?.appleTouchIconUrl} />
+      <link rel="icon" href={meta?.favIconSvgUrl} type="image/svg+xml" />
+      <link rel="manifest" href={meta?.manifestUrl} />
+    </Head>
+  );
+};
+
+export default VenuePageMeta;

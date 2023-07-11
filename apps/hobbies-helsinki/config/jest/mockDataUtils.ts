c92c54592d1e90befe35ac7527a20ecd1526cd09
Niko Lindroos <niko.j.makela@gmail.com>
2023-07-11 22:05:27 +0300

    refactor: deprecated faker sample word usage

--- apps/hobbies-helsinki/config/jest/mockDataUtils.ts
+++ apps/hobbies-helsinki/config/jest/mockDataUtils.ts
@@ -99,7 +99,7 @@ export const fakeTargetGroup = (overrides?: Partial<Audience>): Audience => {
       id: faker.string.uuid(),
       internalContext: '',
       internalId: faker.string.uuid(),
-      name: fakeLocalizedObject(faker.random.word()),
+      name: fakeLocalizedObject(faker.word.sample()),
     },
     overrides
   );

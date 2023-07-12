f6485d40ad8f5e09bfe13575e79570b814b93f10
Ivan Melnik <melnik.job@gmail.com>
2023-07-12 14:17:32 +0200

    fix: typings hcrc

--- packages/components/src/components/footer/Footer.tsx
+++ packages/components/src/components/footer/Footer.tsx
@@ -71,7 +71,7 @@ const FooterSection: FunctionComponent<FooterSectionProps> = ({
           {footerMenu?.menuItems?.nodes?.map(
             // NOTE: HCRC-build sometimes fails - this type should not be needed.
             // : Menu['menuItems']['nodes'][number]
-            (navigationItem: Menu['menuItems']['nodes'][number]) => (
+            (navigationItem) => (
               <Footer.Item
                 className={styles.footerLink}
                 key={navigationItem?.id}

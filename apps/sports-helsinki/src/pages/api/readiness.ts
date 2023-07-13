b44e783157f6551a1c9243f4c0b1c9d6e5475afb
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-07-13 14:45:12 +0300

    fix: more release information on readiness TC-65

--- apps/sports-helsinki/src/pages/api/readiness.ts
+++ apps/sports-helsinki/src/pages/api/readiness.ts
@@ -1,6 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  const version = process.env.NEXT_PUBLIC_VERSION;
-  res.status(200).json({ status: 'ok', version: `${version}` });
+  const packageVersion = process.env.APP_VERSION;
+  const release = process.env.NEXT_PUBLIC_RELEASE;
+  const commitHash = process.env.NEXT_PUBLIC_COMMITHASH;
+  const buildTime = process.env.BUILD_TIME;
+
+  res.status(200).json({
+    status: 'ok',
+    release: `${release}`,
+    packageVersion: `${packageVersion}`,
+    commitHash: `${commitHash}`,
+    buildTime: `${buildTime}`,
+  });
 }

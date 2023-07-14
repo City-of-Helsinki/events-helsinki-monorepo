01c869c2ca0d0b0e6fddf96bc38af3f73da6e09e
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-07-14 09:29:34 +0300

    chore: refactor readiness api

--- apps/sports-helsinki/src/pages/api/readiness.ts
+++ apps/sports-helsinki/src/pages/api/readiness.ts
@@ -1,16 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  const packageVersion = process.env.APP_VERSION;
-  const release = process.env.NEXT_PUBLIC_RELEASE;
-  const commitHash = process.env.NEXT_PUBLIC_COMMITHASH;
-  const buildTime = process.env.BUILD_TIME;
+  const packageVersion = process.env.APP_VERSION ?? '';
+  const release = process.env.NEXT_PUBLIC_RELEASE ?? '';
+  const commitHash = process.env.NEXT_PUBLIC_COMMITHASH ?? '';
+  const buildTime = process.env.BUILD_TIME ?? '';
 
   res.status(200).json({
     status: 'ok',
-    release: `${release}`,
-    packageVersion: `${packageVersion}`,
-    commitHash: `${commitHash}`,
-    buildTime: `${buildTime}`,
+    release,
+    packageVersion,
+    commitHash,
+    buildTime,
   });
 }

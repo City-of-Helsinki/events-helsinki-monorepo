19720c3db471a4a62886c6ac771fcdc2ba4c9774
Niko Lindroos <niko.j.makela@gmail.com>
2023-08-22 12:14:09 +0300

    feat(proxy): fetch venue details with fallback languages
    LIIKUNTA-547.
    If the locale that is set in header does not return any results,
    use the configured fallback languages.

---
+++ proxies/venue-graphql-proxy/src/utils/__tests__/utils.test.ts
@@ -0,0 +1,76 @@
+import AppConfig from '../../config/AppConfig';
+import type VenueContext from '../../context/VenueContext';
+import type { TranslationsObject, Locale, VenueDetails } from '../../types';
+import { pickLocaleWithFallback, translateVenue } from '../utils';
+
+const fi = 'tekstiä suomeksi';
+const sv = 'text på svenska';
+const en = 'text in English';
+
+describe('pickLocaleWithFallback', () => {
+  it.each<[Locale, Locale, TranslationsObject]>([
+    ['sv', 'en', { sv }],
+    ['fi', 'en', { fi, sv }],
+    ['en', 'sv', { fi, en }],
+    ['fi', 'sv', { fi }],
+  ])(
+    'uses "%s" as first fallback locale to fetch the details when the default locale "%s" is not available in object %o',
+    (resultLocale, locale, obj) => {
+      const translation = pickLocaleWithFallback(obj, locale);
+      const expectedTranslation = obj[resultLocale];
+      expect(translation).toStrictEqual(expectedTranslation);
+    }
+  );
+});
+
+describe('translateVenue', () => {
+  it.each(AppConfig.supportedLocales)(
+    'returns the details in given language %s',
+    (locale) => {
+      const venueData = {
+        name: {
+          fi,
+          en,
+          sv,
+        },
+        description: {
+          fi,
+          en,
+          sv,
+        },
+      } as Partial<VenueDetails>;
+      const context = { language: locale } as VenueContext;
+      const translatedVenue = translateVenue(venueData, context);
+      expect(translatedVenue.description).toStrictEqual(
+        venueData.description![locale]
+      );
+      expect(translatedVenue.name).toStrictEqual(venueData.name![locale]);
+    }
+  );
+  it.each(AppConfig.supportedLocales)(
+    'uses a fallback language when the requested details are not available in given language %s',
+    (locale) => {
+      const venueData = {
+        name: {
+          fi,
+          en,
+          sv,
+        },
+        description: {
+          fi,
+          en,
+          sv,
+        },
+      } as Partial<VenueDetails>;
+      const fallbackLng = locale === 'en' ? 'fi' : 'en';
+      const context = { language: locale } as VenueContext;
+      delete venueData.description![locale];
+      delete venueData.name![locale];
+      const translatedVenue = translateVenue(venueData, context, true);
+      expect(translatedVenue.description).toStrictEqual(
+        venueData.description![fallbackLng]
+      );
+      expect(translatedVenue.name).toStrictEqual(venueData.name![fallbackLng]);
+    }
+  );
+});

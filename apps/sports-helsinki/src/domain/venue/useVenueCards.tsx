d0811884b985837d9f53c1db456ab15ac482d250
Niko Lindroos <niko.j.makela@gmail.com>
2023-08-29 10:24:01 +0300

    fix: similar venues for an empty result from the venues proxy
    LIIKUNTA-551

--- apps/sports-helsinki/src/domain/venue/useVenueCards.tsx
+++ apps/sports-helsinki/src/domain/venue/useVenueCards.tsx
@@ -23,31 +23,33 @@ function useVenueCards({ venues, returnPath }: useVenueCardsProps) {
     components: { VenueCardContent },
   } = useConfig();
   return (
-    venues?.map((venue, i) => {
-      const cardProps = getLocationCardProps(venue);
-      // NOTE: The venue-graphql-proxy seems to always need a TPREK-id,
-      // so the id's that has any other source, needs to be converted.
-      const venueId = getVenueSourceId(
-        getVenueIdParts(venue.id).id,
-        Sources.TPREK
-      );
-      const url = routerHelper.getLocalizedCmsItemUrl(
-        ROUTES.VENUES,
-        { venueId, returnPath },
-        locale
-      );
-      return (
-        <Card
-          key={cardProps.id}
-          {...cardProps}
-          url={url}
-          direction="fixed-vertical"
-          customContent={
-            VenueCardContent && <VenueCardContent location={venues[i]} />
-          }
-        />
-      );
-    }) ?? []
+    venues
+      ?.filter((venue) => !!venue.id)
+      ?.map((venue, i) => {
+        const cardProps = getLocationCardProps(venue);
+        // NOTE: The venue-graphql-proxy seems to always need a TPREK-id,
+        // so the id's that has any other source, needs to be converted.
+        const venueId = getVenueSourceId(
+          getVenueIdParts(venue.id).id,
+          Sources.TPREK
+        );
+        const url = routerHelper.getLocalizedCmsItemUrl(
+          ROUTES.VENUES,
+          { venueId, returnPath },
+          locale
+        );
+        return (
+          <Card
+            key={cardProps.id}
+            {...cardProps}
+            url={url}
+            direction="fixed-vertical"
+            customContent={
+              VenueCardContent && <VenueCardContent location={venues[i]} />
+            }
+          />
+        );
+      }) ?? []
   );
 }
 

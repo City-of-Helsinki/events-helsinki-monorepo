1d97c39b00074a9f6881452958a6cdcb8d9a1388
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-18 19:19:44 +0300

    feat(search): sync the venue search ordering with the accessibility profile
    LIIKUNTA-568 LIIKUNTA-569 LIIKUNTA-570.
    
    When the accessibility profile is selected in the search context,
    the venue ordering should always be a orderByAccessibilityProfile
    with the current accessibility profile.
    
    This can be achieved by controlling the venueOrderBy-parameter of the search form.
    Every time when the accessibility profile is set and it does not match the venueOrderBy,
    the venueOrderBy is set and the URL is refreshed.
    
    Also, when the accessibility profile is set and the ordering should be forced
    to accessibility profile, the ordering selector is disabled.

--- apps/sports-helsinki/src/hooks/useHandleUnifiedSearchOrderChange.ts
+++ apps/sports-helsinki/src/hooks/useHandleUnifiedSearchOrderChange.ts
@@ -1,5 +1,6 @@
 import {
   UnifiedSearchOrderBy,
+  isAccessibilityProfile,
   useGeolocation,
 } from '@events-helsinki/components';
 import type {
@@ -9,45 +10,68 @@ import type {
   Coordinates,
   Option,
 } from '@events-helsinki/components';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useCombinedSearchContext } from '../domain/search/combinedSearch/adapters/CombinedSearchContext';
 
 const useHandleUnifiedSearchOrderChange = () => {
   const geolocation: GeolocationContextType = useGeolocation({ skip: true });
-  const { setFormValues, updateRouteToSearchPage } = useCombinedSearchContext();
+  const {
+    setFormValues,
+    updateRouteToSearchPage,
+    formValues: { accessibilityProfile, venueOrderBy },
+  } = useCombinedSearchContext();
+
+  // Sync the accessibility profile search form value with the venue search query parameter
+  React.useEffect(() => {
+    if (accessibilityProfile !== venueOrderBy) {
+      setFormValues({
+        venueOrderBy: accessibilityProfile,
+      });
+      // Update the URL
+      updateRouteToSearchPage({ shallow: true });
+    }
+  }, [accessibilityProfile, venueOrderBy]);
+
+  // Provide a callback for onChange-handler
   return useCallback(
     async (option: Option) => {
       const [orderBy, orderDir] = option.value.split('-') as [
         UnifiedSearchOrderByType,
         OrderDirType
       ];
+      if (isAccessibilityProfile(option.value)) {
+        // Update the combined search form context
+        setFormValues({
+          venueOrderBy: option.value,
+        });
+      } else {
+        // If the user wants to order by distance, try and resolve geolocation
+        if (orderBy === UnifiedSearchOrderBy.distance) {
+          let location: Coordinates | null = geolocation.coordinates;
 
-      // If the user wants to order by distance, try and resolve geolocation
-      if (orderBy === UnifiedSearchOrderBy.distance) {
-        let location: Coordinates | null = geolocation.coordinates;
+          if (!geolocation.called || !geolocation.coordinates) {
+            // Wait until position is resolved. This defers querying search
+            // results until location is resolved, which will result in less UI
+            // states and a slightly better UX.
+            location = await geolocation.resolve();
+          }
 
-        if (!geolocation.called || !geolocation.coordinates) {
-          // Wait until position is resolved. This defers querying search
-          // results until location is resolved, which will result in less UI
-          // states and a slightly better UX.
-          location = await geolocation.resolve();
+          // If location could not be found, return early and do not change
+          // ordering.
+          if (!location) {
+            return null;
+          }
         }
 
-        // If location could not be found, return early and do not change
-        // ordering.
-        if (!location) {
-          return null;
-        }
+        // Update the combined search form context
+        setFormValues({
+          venueOrderBy: orderDir === 'desc' ? `-${orderBy}` : orderBy,
+        });
       }
-
-      // Update the combined search form context
-      setFormValues({
-        venueOrderBy: orderDir === 'desc' ? `-${orderBy}` : orderBy,
-      });
       // Update the URL
       updateRouteToSearchPage({ shallow: true });
     },
-    [geolocation]
+    [geolocation, accessibilityProfile]
   );
 };
 

d79ede2eedfef6054d32c8fddb0bf1c156954c5c
Niko Lindroos <niko.j.makela@gmail.com>
2023-11-10 16:27:46 +0200

    refactor: backward compatibility urls
    LIIKUNTA-586.
    
    The old campaign url redirects are now in their own file
    and the new backward compatibility urls are in their own too.
    
    Since the new backward compatibility urls are all Finnish
    they now works with the following root paths:
    - /pages
    - /sivut
    - /fi/pages
    - /fi/sivut
    
    The destination is always /fi/sivut/...

--- apps/sports-helsinki/next.config.js
+++ apps/sports-helsinki/next.config.js
@@ -1,23 +1,39 @@
 const packageJson = require('./package.json');
 const i18nRoutes = require('./i18nRoutes.config');
 const { i18n } = require('./next-i18next.config');
-const redirectRoutes = require('./redirectRoutes.config');
+const redirectCampaignRoutes = require('./redirectCampaignRoutes.config');
+const redirectBackwardCompatibilityRoutes = require('./redirectBackwardCompatibilityRoutes.config');
 const nextBaseConfig = require('../../next.base.config');
+
+const campaignRoutes = Object.entries(redirectCampaignRoutes).flatMap(
+  ([source, destination]) => [
+    {
+      // Not setting `locale: false` here because that didn't work for some reason.
+      // When locale is left undefined middleware.ts's prefixDefaultLocale function
+      // lets these redirect routes through without prefixing them with locale.
+      source,
+      destination,
+      permanent: true,
+    },
+  ]
+);
+
+const backwardRoutes = Object.entries(
+  redirectBackwardCompatibilityRoutes
+).flatMap(([source, destination]) => [
+  {
+    source,
+    destination,
+    permanent: true,
+    locale: false,
+  },
+]);
 
 module.exports = nextBaseConfig({
   packageJson,
   i18nRoutes,
   i18n,
   async redirects() {
-    return Object.entries(redirectRoutes).flatMap(([source, destination]) => [
-      {
-        // Not setting `locale: false` here because that didn't work for some reason.
-        // When locale is left undefined middleware.ts's prefixDefaultLocale function
-        // lets these redirect routes through without prefixing them with locale.
-        source,
-        destination,
-        permanent: true,
-      },
-    ]);
+    return [...campaignRoutes, ...backwardRoutes];
   },
 });

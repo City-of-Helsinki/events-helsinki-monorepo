City-of-Helsinki/events-helsinki-monorepo

commit 58ee85254bfe54a36b94de68d0a4039d98ef21bc

fix(navigation): fix retaining URL search parameters on language change
The problem was that router.query had no query parameters, even if the
current URL did when serving server-side pages. Using
window.location.search if it is available and router.query only as a
fallback fixes this.

refs LIIKUNTA-597

packages/components/src/components/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import type { ArticleType, PageType } from 'react-helsinki-headless-cms';
 import {
   Navigation as RHHCNavigation,
@@ -43,6 +43,17 @@ export default function Navigation({
     languages ??
     languagesQuery.data?.languages?.filter(isLanguage);
 
+  // router.query has no query parameters, even if the current URL does when serving
+  // server-side generated pages. Using window.location.search when available and not
+  // always relying on router.query makes the query parameters more available.
+  const getCurrentParsedUrlQuery = useCallback(
+    () =>
+      window
+        ? Object.fromEntries(new URLSearchParams(window.location.search))
+        : router.query,
+    [router?.query]
+  );
+
   return (
     <>
       <RHHCNavigation
@@ -73,7 +84,7 @@ export default function Navigation({
                       cmsHelper.removeContextPathFromUri(translatedPage.uri)
                     ) ?? '',
                 }
-              : router.query,
+              : getCurrentParsedUrlQuery(),
             slug as AppLanguage
           );
         }}

commit 43f6c4007c74e1dbf7d5f2a6fab8efecdf381e04

fix(navigation): fix replacing [eventId] in URL when changing languages
fix bug that was introduced in 58ee85254bfe54a36b94de68d0a4039d98ef21bc

refs LIIKUNTA-597

packages/components/src/components/navigation/Navigation.tsx
@@ -44,14 +44,17 @@ export default function Navigation({
     languagesQuery.data?.languages?.filter(isLanguage);
 
   // router.query has no query parameters, even if the current URL does when serving
-  // server-side generated pages. Using window.location.search when available and not
-  // always relying on router.query makes the query parameters more available.
+  // server-side generated pages. Simply using window.location.search always when
+  // available broke e.g. /courses/[eventId] URL part so that the [eventId] part didn't
+  // get replaced with the actual event ID. Merging both query sources worked better.
   const getCurrentParsedUrlQuery = useCallback(
-    () =>
-      window
+    () => ({
+      ...router.query,
+      ...(window
         ? Object.fromEntries(new URLSearchParams(window.location.search))
-        : router.query,
-    [router?.query]
+        : {}),
+    }),
+    [router.query]
   );
 
   return (
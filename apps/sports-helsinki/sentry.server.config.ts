d909ae96344378039e657da324bc65f2adbece0b
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-22 13:33:17 +0200

    feat(apps): configure sentry in use
    LIIKUNTA-583

--- apps/sports-helsinki/sentry.server.config.ts
+++ apps/sports-helsinki/sentry.server.config.ts
@@ -2,16 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { init as sentryInit } from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs';
 
-sentryInit({
+Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
   // Adjust this value in production, or use tracesSampler for greater control
   // @see https://develop.sentry.dev/sdk/performance/
   // To turn it off, remove the line
   // @see https://github.com/getsentry/sentry-javascript/discussions/4503#discussioncomment-2143116
-  tracesSampleRate: 1.0,
+  tracesSampleRate: parseFloat(
+    process.env.NEXT_PUBLIC_SENTRY_TRACE_SAMPLE_RATE ?? '0.1'
+  ),
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so

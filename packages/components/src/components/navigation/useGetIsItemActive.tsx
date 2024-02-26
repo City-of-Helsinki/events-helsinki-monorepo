4220f9dd170a53a146133dffd63b7e20e740177c
Niko Lindroos <niko.j.makela@gmail.com>
2024-02-26 19:23:27 +0200

    fix: parent menu item activity indicator in Swedish and English
    LIIKUNTA-617

--- packages/components/src/components/navigation/useGetIsItemActive.tsx
+++ packages/components/src/components/navigation/useGetIsItemActive.tsx
@@ -12,24 +12,35 @@ export default function useGetItemIsActive(): NonNullable<
   const routerHelper = useCmsRoutedAppHelper();
   const locale = useLocale();
 
-  return ({ path }) => {
+  return ({ path }): boolean => {
     const pathWithoutTrailingSlash = (path ?? '').replace(/\/$/, '');
     const i18nRouterPathname = routerHelper.getI18nPath(pathname, locale);
     const i18nRouterAsPath = routerHelper.getI18nPath(asPath, locale);
     const [, resolvedUrl] = resolveHref(router, { pathname, query }, true);
     const resolvedPathname = resolvedUrl?.split('?')[0];
-    return Boolean(
-      // The router.pathname needs to be checked when dealing with "statically routed page".
+    const parentPathnamePattern = i18nRouterPathname.split('[')[0];
+
+    // The router.pathname needs to be checked when dealing with "statically routed page".
+    const isStaticallyRoutedItem =
       pathWithoutTrailingSlash === i18nRouterPathname ||
-        pathWithoutTrailingSlash === `/${locale}${i18nRouterPathname}` ||
-        // The pathname may or may not contain the i18n version of the menu item path
-        pathWithoutTrailingSlash === resolvedPathname ||
-        pathWithoutTrailingSlash === `/${locale}${resolvedPathname}` ||
-        // Since the menu can contain subitems in a dropdown, the parent items needs to be checked too.
-        // NOTE: We are now assuming that all the parent items are also real parent pages.
-        (path &&
-          (i18nRouterAsPath.startsWith(path) ||
-            resolvedPathname?.startsWith(path)))
+      pathWithoutTrailingSlash === `/${locale}${i18nRouterPathname}`;
+
+    // The pathname may or may not contain the i18n version of the menu item path
+    const isI18nVersionItem =
+      pathWithoutTrailingSlash === resolvedPathname ||
+      pathWithoutTrailingSlash === `/${locale}${resolvedPathname}`;
+
+    // Since the menu can contain subitems in a dropdown, the parent items needs to be checked too.
+    // NOTE: We are now assuming that all the parent items are also real parent pages.
+    const isParentItem = Boolean(
+      path &&
+        (i18nRouterAsPath.startsWith(path) ||
+          parentPathnamePattern.startsWith(path) ||
+          `/${locale}${parentPathnamePattern}`.startsWith(path) ||
+          resolvedPathname?.startsWith(path) ||
+          `/${locale}${resolvedPathname}`.startsWith(path))
     );
+
+    return isStaticallyRoutedItem || isI18nVersionItem || isParentItem;
   };
 }

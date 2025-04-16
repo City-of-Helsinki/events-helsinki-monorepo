654d537878f7b85068d2ad035721c31df3a02107
Kari Salminen <kari.salminen@anders.com>
2025-04-16 11:09:17 +0300

    feat(events): add /merellinenhelsinki campaign redirect to tapahtumat
    also sort campaign redirects to alphabetical order
    
    refs TH-1371

--- apps/events-helsinki/redirectCampaignRoutes.config.js
+++ apps/events-helsinki/redirectCampaignRoutes.config.js
@@ -2,8 +2,9 @@
 // prettier-ignore
 const redirectCampaignRoutes = {
   // Finnish locale versions
-  '/maaginen': '/fi/articles/yleinen/maaginen-viikko-helsingin-kirjastoissa',
   '/ekoviikko': '/fi/articles/yleinen/ekoviikko-kirjastoissa',
+  '/maaginen': '/fi/articles/yleinen/maaginen-viikko-helsingin-kirjastoissa',
+  '/merellinenhelsinki': '/fi/articles/yleinen/merellinen-helsinki-viikonloppu-2025',
 
   // Swedish locale versions
   // None yet

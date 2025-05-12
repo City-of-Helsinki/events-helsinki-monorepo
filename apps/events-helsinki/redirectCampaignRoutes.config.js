9ba2cc6308f02691ccfcd8fe14fdf5159088e171
Niko Lindroos <niko.j.makela@gmail.com>
2025-05-12 09:27:00 +0300

    feat: add campaign URL for Koffarin-Kinofest
    TH-1374.

--- apps/events-helsinki/redirectCampaignRoutes.config.js
+++ apps/events-helsinki/redirectCampaignRoutes.config.js
@@ -5,6 +5,7 @@ const redirectCampaignRoutes = {
   '/ekoviikko': '/fi/articles/yleinen/ekoviikko-kirjastoissa',
   '/maaginen': '/fi/articles/yleinen/maaginen-viikko-helsingin-kirjastoissa',
   '/merellinenhelsinki': '/fi/articles/yleinen/merellinen-helsinki-viikonloppu-2025',
+  '/koffarinkinofest': '/fi/articles/elokuva/koffarin-kinofest',
 
   // Swedish locale versions
   // None yet

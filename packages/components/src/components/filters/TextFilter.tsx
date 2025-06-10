68b61d0c54f41726633c1ebeca96c371d319c226
Niko Lindroos <niko.j.makela@gmail.com>
2025-06-10 15:25:28 +0300

    feat(packages): change search text-parameter
    HH-371.
    
    Use "xFullText" as a (default) event search parameter instead of "text".

--- packages/components/src/components/filters/TextFilter.tsx
+++ packages/components/src/components/filters/TextFilter.tsx
@@ -1,13 +1,14 @@
 import { FilterButton } from '../filterButton';
-import type { FilterType } from '../filterButton';
+import type { TextFilterType, FilterType } from '../filterButton';
 
 interface Props {
   onRemove: (value: string, type: FilterType) => void;
   text: string;
+  type: TextFilterType;
 }
 
-const TextFilter: React.FC<Props> = ({ onRemove, text }) => (
-  <FilterButton onRemove={onRemove} text={text} type="text" value={text} />
+const TextFilter: React.FC<Props> = ({ onRemove, text, type = 'text' }) => (
+  <FilterButton onRemove={onRemove} text={text} type={type} value={text} />
 );
 
 export default TextFilter;

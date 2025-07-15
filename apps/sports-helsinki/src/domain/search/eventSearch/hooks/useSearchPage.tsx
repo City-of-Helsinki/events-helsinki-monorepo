832f87f1b46954d6b2381c93e5d38a09f5b4f859
Niko Lindroos <niko.j.makela@gmail.com>
2025-07-15 16:32:42 +0300

    fix(sports): errenous import statement to useCombinedSearchContext
    HH-372 TH-1376 LIIKUNTA-704.
    
    `yarn check:install` started to warn about import statement where a
    shortcut-path was used. It was fine earlier, since there was a package
    called "domain" installed.

--- apps/sports-helsinki/src/domain/search/eventSearch/hooks/useSearchPage.tsx
+++ apps/sports-helsinki/src/domain/search/eventSearch/hooks/useSearchPage.tsx
@@ -11,9 +11,9 @@ import React from 'react';
 import { useConfig } from 'react-helsinki-headless-cms';
 import { scroller } from 'react-scroll';
 import { toast } from 'react-toastify';
-import { useCombinedSearchContext } from 'domain/search/combinedSearch/adapters/CombinedSearchContext';
 import { SEARCH_ROUTES } from '../../../../constants';
 import routerHelper from '../../../../domain/app/routerHelper';
+import { useCombinedSearchContext } from '../../../../domain/search/combinedSearch/adapters/CombinedSearchContext';
 import type { SearchPage } from '../../../../domain/search/combinedSearch/types';
 import { getNextPage } from '../utils';
 

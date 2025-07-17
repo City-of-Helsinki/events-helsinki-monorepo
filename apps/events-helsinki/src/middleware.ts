2bb65a6a166d3b8c4cb3ee868f81ebd3d5804f92
Niko Lindroos <niko.j.makela@gmail.com>
2025-07-17 15:39:58 +0300

    feat(events): short URL redirects
    TH-1377.
    
    Add short URL redirects feature to Events app. The CMS users can then
    create short URLs from the CMS admin panel, similarly as in Sports app.

--- apps/events-helsinki/src/middleware.ts
+++ apps/events-helsinki/src/middleware.ts
@@ -1,13 +1,31 @@
 import {
   prefixDefaultLocale,
+  RedirectHandler,
   shouldBypassMiddlewareAsStaticRequest,
+  updateRedirectsCache,
 } from '@events-helsinki/components/edge-runtime-compatible/middlewares';
 import type { NextRequest } from 'next/server';
+
+import redirectCampaignRoutes from '../redirectCampaignRoutes.config.mjs';
+
+const redirectHandler = new RedirectHandler({
+  redirectCampaignRoutes,
+});
 
 export async function middleware(req: NextRequest) {
   if (shouldBypassMiddlewareAsStaticRequest(req)) {
     return;
   }
 
+  const redirectResponse = redirectHandler.handleRedirects(req);
+  if (redirectResponse) {
+    return redirectResponse;
+  }
+
   return prefixDefaultLocale(req);
 }
+
+/**
+ * Initialize redirects cache immediately and schedule its updating.
+ */
+setTimeout(updateRedirectsCache, 0);

88ca7d32ef17411db425971426e4deb02cddb4ff
Niko Lindroos <niko.j.makela@gmail.com>
2025-09-04 16:26:09 +0300

    style(sonar): rename "todo" to "note" to ignore task completion
    TH-1409

--- apps/sports-helsinki/src/domain/event/useSimilarEventsQueryVariables.tsx
+++ apps/sports-helsinki/src/domain/event/useSimilarEventsQueryVariables.tsx
@@ -29,7 +29,7 @@ const useSimilarEventsQueryVariables = (event: EventFields) => {
     return {
       ...getEventSearchVariables({
         include: ['keywords', 'location'],
-        // TODO: use SIMILAR_EVENTS_AMOUNT when LinkedEvents-query
+        // NOTE: use SIMILAR_EVENTS_AMOUNT when LinkedEvents-query
         // with keyword_OR_set* -param is fixed and it returns distinct results
         pageSize: 100,
         params: new URLSearchParams(searchParams),

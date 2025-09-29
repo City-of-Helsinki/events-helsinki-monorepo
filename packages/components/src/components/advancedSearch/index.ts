68264d8616b19dff33270f691925e7e3691e18cd
Kari Salminen <kari.salminen@anders.com>
2025-09-29 16:57:25 +0300

    feat: add AdvancedSearchNumberInput, use it for selecting age in hobbies
    Automatically limit age input to range 0–130. The upper limit is a
    realistic value as 122 years is the oldest person documented to have
    lived on earth according to Wikipedia:
    https://en.wikipedia.org/wiki/List_of_the_verified_oldest_people
    
    Also:
    - Fix broken variable mapping in FilterSummary where maximum age was
      mapped to publisher, now publisher is mapped to publisher.
    
    refs HH-381

--- packages/components/src/components/advancedSearch/index.ts
+++ packages/components/src/components/advancedSearch/index.ts
@@ -1,2 +1,4 @@
 export { default as useMoveFocusFromInputToContainer } from './useMoveFocusFromInputToContainer';
 export { default as AdvancedSearchTextInput } from './AdvancedSearchTextInput';
+export { default as AdvancedSearchNumberInput } from './AdvancedSearchNumberInput';
+export { type AdvancedSearchNumberInputProps } from './AdvancedSearchNumberInput';

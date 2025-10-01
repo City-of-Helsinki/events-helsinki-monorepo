e4274ce5fd54242d77dfb49f4adb0b0f6f866208
Kari Salminen <kari.salminen@anders.com>
2025-10-01 12:57:35 +0300

    feat(components): add optional icon parameter to AdvancedSearchTextInput
    Now the icon can be overridden in AdvancedSearchTextInput component
    similarly as in AdvancedSearchNumberInput.
    
    refs HH-381

--- packages/components/src/components/advancedSearch/index.ts
+++ packages/components/src/components/advancedSearch/index.ts
@@ -1,4 +1,5 @@
 export { default as useMoveFocusFromInputToContainer } from './useMoveFocusFromInputToContainer';
 export { default as AdvancedSearchTextInput } from './AdvancedSearchTextInput';
+export { type AdvancedSearchTextInputProps } from './AdvancedSearchTextInput';
 export { default as AdvancedSearchNumberInput } from './AdvancedSearchNumberInput';
 export { type AdvancedSearchNumberInputProps } from './AdvancedSearchNumberInput';

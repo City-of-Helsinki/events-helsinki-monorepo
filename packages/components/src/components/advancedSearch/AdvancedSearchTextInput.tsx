13568016b8361b0a5ff3c01efd15b4990efd1f34
Kari Salminen <kari.salminen@anders.com>
2025-10-01 12:57:35 +0300

    feat(components): add optional icon parameter to AdvancedSearchTextInput
    Now the icon can be overridden in AdvancedSearchTextInput component
    similarly as in AdvancedSearchNumberInput.
    
    refs HH-381

--- packages/components/src/components/advancedSearch/AdvancedSearchTextInput.tsx
+++ packages/components/src/components/advancedSearch/AdvancedSearchTextInput.tsx
@@ -3,17 +3,24 @@ import { IconSearch, TextInput } from 'hds-react';
 import React from 'react';
 import AdvancedSearchInput from './AdvancedSearchInput';
 
+export type AdvancedSearchTextInputProps = TextInputProps & {
+  icon?: Parameters<typeof AdvancedSearchInput>[0]['IconComponent'];
+};
+
 /**
  * AdvancedSearchTextInput is a component that provides a search input field
- * with a search icon. It is a pre-configured version of the AdvancedSearchInput
- * component, using HDS TextInput and IconSearch.
- * @param props The props for the HDS TextInput component.
+ * with an icon—defaulting to a search icon if no other icon is specified.
+ * It is a pre-configured version of the AdvancedSearchInput component, using
+ * HDS TextInput and IconSearch.
+ * @param props The props for the HDS TextInput component and optional icon.
  * @returns The rendered component.
  */
-function AdvancedSearchTextInput(props: TextInputProps): React.ReactElement {
+function AdvancedSearchTextInput(
+  props: AdvancedSearchTextInputProps
+): React.ReactElement {
   return (
     <AdvancedSearchInput
-      IconComponent={<IconSearch />}
+      IconComponent={props.icon ?? <IconSearch />}
       InputComponent={<TextInput {...props} type="text" autoComplete="off" />}
     />
   );

51f268e349449f8b88699440dca973d3fc051be9
Kari Salminen <kari.salminen@anders.com>
2025-10-08 18:06:15 +0300

    fix(tests): hide error msg caused by HDS 3 Select in packages/components
    Hide error message caused by hds-react v3 Select component.
    
    Example use case:
    - SearchSelect (packages/components) → Select (packages/components) →
      Select (hds-react)
    
    Related issue:
    - https://github.com/facebook/react/issues/29233
    
    Example output from test run:
    ```
    $ yarn test src/components/select/__tests__/SearchSelect.test.tsx
    
     RUN  v3.2.4 packages/components
    
    stderr | src/components/select/__tests__/SearchSelect.test.tsx >
    SearchSelect > successful cases > renders properly
    
    Warning: Li: Support for defaultProps will be removed from function
    components in a future major release. Use JavaScript default parameters
    instead.
    at Li (node_modules\hds-react\cjs\index.js:1:318591)
    at Select (packages\components\src\components\select\Select.tsx:11:3)
    at SearchSelect (packages\components\src\components\select\SearchSele...
    ```
    
    refs TH-1437

--- packages/components/vitest-setup.ts
+++ packages/components/vitest-setup.ts
@@ -1,3 +1,4 @@
+import { hideConsoleMessages } from '@events-helsinki/common-tests';
 import { loadEnvConfig } from '@next/env';
 
 import { expect } from 'vitest';
@@ -83,3 +84,19 @@ vi.mock('next-i18next', async () => {
 });
 
 loadEnvConfig(process.cwd());
+
+hideConsoleMessages({
+  error: [
+    // Hide error message caused by hds-react v3:
+    // eslint-disable-next-line @stylistic/max-len
+    // https://github.com/City-of-Helsinki/helsinki-design-system/blob/v3.11.0/packages/react/src/components/dropdown/select/Select.tsx#L669
+    //
+    // Example use case:
+    // SearchSelect (packages/components) ? Select (packages/components) ? Select (hds-react)
+    // Removing this hiding and running SearchSelect tests should show this error if HDS v3.11.0 is still used.
+    //
+    // Related issue:
+    // https://github.com/facebook/react/issues/29233
+    /Support for defaultProps will be removed.*Use JavaScript default parameters instead.*hds-react/s,
+  ],
+});

52fb0af3934791422101c4c9a64cc7b6e31da60b
Kari Salminen <kari.salminen@anders.com>
2025-10-08 17:02:48 +0300

    fix(tests): match mocks in EventContent tests
    By matching mocks the warnings about not having mocks available for
    made requests are removed → cleaner test run output.
    
    refs TH-1437

--- packages/components/config/tests/index.ts
+++ packages/components/config/tests/index.ts
@@ -1,4 +1,5 @@
 export * from './initI18n';
 export * from './mockDataUtils';
+export * from './mocks';
 export * from './test-utils';
 export { I18nextTestStubProvider } from './I18nextTestStubProvider';

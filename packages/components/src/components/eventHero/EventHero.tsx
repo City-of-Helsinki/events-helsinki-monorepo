9e458b1b8526016c26f4091e607a242236631156
Niko Lindroos <niko.j.makela@gmail.com>
2025-10-15 16:09:41 +0300

    refactor(components): split EventHero component
    HH-449.
    
    Split the EventHero component to multiple private function based
    components, for sake of clarity.

--- packages/components/src/components/eventHero/EventHero.tsx
+++ packages/components/src/components/eventHero/EventHero.tsx
@@ -44,33 +44,134 @@ export type EventHeroProps = {
   withActions?: boolean;
 };
 
+const ShortDescription: React.FC<Pick<EventHeroProps, 'event'>> = ({
+  event,
+}) => {
+  const locale = useLocale();
+  const { shortDescription } = getEventFields(event, locale);
+  return (
+    <>
+      {shortDescription && (
+        <div className={styles.description}>{shortDescription}</div>
+      )}
+    </>
+  );
+};
+
+const LocationInfo: React.FC<Pick<EventHeroProps, 'event'>> = ({ event }) => (
+  <div className={styles.location}>
+    <InfoWithIcon icon={<IconLocation aria-hidden />} title={''}>
+      <EventLocationText
+        event={event}
+        showNeighborhood={false}
+        showLocationName={true}
+      />
+    </InfoWithIcon>
+  </div>
+);
+
+const TimeInfo: React.FC<Pick<EventHeroProps, 'event' | 'superEvent'>> = ({
+  event,
+  superEvent,
+}) => {
+  const { t: commonTranslation } = useTranslation('common');
+  const locale = useLocale();
+  const {
+    endTime: eventEndTime,
+
+    startTime: eventStartTime,
+  } = getEventFields(event, locale);
+  const startTime =
+    superEvent?.status === 'pending'
+      ? ''
+      : superEvent?.data?.startTime || eventStartTime;
+  const endTime =
+    superEvent?.status === 'pending'
+      ? ''
+      : superEvent?.data?.endTime || eventEndTime;
+
+  return (
+    <div className={styles.start}>
+      {(startTime !== eventStartTime || endTime !== eventEndTime) && (
+        <InfoWithIcon icon={<IconCalendarClock aria-hidden />} title={''}>
+          {superEvent?.status === 'pending' ? (
+            <SkeletonLoader />
+          ) : (
+            getDateRangeStr({
+              start: eventStartTime || '',
+              end: eventEndTime,
+              locale,
+              includeTime: true,
+              timeAbbreviation: commonTranslation('timeAbbreviation'),
+            })
+          )}
+        </InfoWithIcon>
+      )}
+    </div>
+  );
+};
+
+const EventPriceInfo: React.FC<Pick<EventHeroProps, 'event'>> = ({ event }) => {
+  const locale = useLocale();
+  const { t } = useTranslation('event');
+  const eventPriceText = getEventPrice(event, locale, t('hero.offers.isFree'));
+
+  return (
+    <>
+      {eventPriceText && (
+        <div className={styles.price}>
+          <InfoWithIcon icon={<IconTicket aria-hidden />} title={''}>
+            {eventPriceText}
+          </InfoWithIcon>
+        </div>
+      )}
+    </>
+  );
+};
+
+const OfferButton: React.FC<Pick<EventHeroProps, 'event'>> = ({ event }) => {
+  const locale = useLocale();
+  const { t } = useTranslation('event');
+
+  const { defaultButtonTheme: theme, defaultButtonVariant: variant } =
+    useAppThemeContext();
+
+  const { offerInfoUrl } = getEventFields(event, locale);
+  const buttonText = getEventHeroButtonText(event, 'button', t);
+  const buttonAriaLabelText = getEventHeroButtonText(event, 'ariaLabel', t);
+  return (
+    <>
+      {offerInfoUrl && (
+        <div className={styles.registrationButtonWrapper}>
+          <Button
+            theme={theme}
+            variant={variant}
+            className={buttonStyles.buttonCoatBlue}
+            aria-label={buttonAriaLabelText}
+            onClick={() => window.open(offerInfoUrl)}
+            iconRight={<IconLinkExternal aria-hidden />}
+          >
+            {buttonText}
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
 const EventHero: React.FC<EventHeroProps> = ({
   event,
   superEvent,
   withActions = true,
 }) => {
   const { t } = useTranslation('event');
-  const { t: commonTranslation } = useTranslation('common');
   const { fallbackImageUrls } = useConfig();
   const locale = useLocale();
   const router = useRouter();
   const search = router.asPath.split('?')[1];
-  const { defaultButtonTheme: theme, defaultButtonVariant: variant } =
-    useAppThemeContext();
 
-  const {
-    endTime: eventEndTime,
-    imageUrl,
-    keywords,
-    offerInfoUrl,
-    shortDescription,
-    startTime: eventStartTime,
-    today,
-    thisWeek,
-  } = getEventFields(event, locale);
-  const eventPriceText = getEventPrice(event, locale, t('hero.offers.isFree'));
-  const buttonText = getEventHeroButtonText(event, 'button', t);
-  const buttonAriaLabelText = getEventHeroButtonText(event, 'ariaLabel', t);
+  const { imageUrl, keywords, today, thisWeek } = getEventFields(event, locale);
+
   const showKeywords = Boolean(today || thisWeek || keywords.length);
   const returnParam = extractLatestReturnPath(search, `/${locale}`);
 
@@ -81,15 +182,6 @@ const EventHero: React.FC<EventHeroProps> = ({
     const goBackUrlLocale = getLocaleFromPathname(goBackUrl);
     router.push(goBackUrl, undefined, { locale: goBackUrlLocale });
   };
-
-  const startTime =
-    superEvent?.status === 'pending'
-      ? ''
-      : superEvent?.data?.startTime || eventStartTime;
-  const endTime =
-    superEvent?.status === 'pending'
-      ? ''
-      : superEvent?.data?.endTime || eventEndTime;
 
   return (
     <PageSection className={classNames(styles.heroSection)}>
@@ -119,68 +211,12 @@ const EventHero: React.FC<EventHeroProps> = ({
                 <h1 className={styles.title} data-testid="event-name">
                   <EventName event={event} />
                 </h1>
-                {shortDescription && (
-                  <div className={styles.description}>{shortDescription}</div>
-                )}
+                <ShortDescription event={event} />
                 <div className={styles.additionalInfo}>
-                  <div className={styles.location}>
-                    <InfoWithIcon
-                      icon={<IconLocation aria-hidden />}
-                      title={''}
-                    >
-                      <EventLocationText
-                        event={event}
-                        showNeighborhood={false}
-                        showLocationName={true}
-                      />
-                    </InfoWithIcon>
-                  </div>
-                  <div className={styles.start}>
-                    {(startTime !== eventStartTime ||
-                      endTime !== eventEndTime) && (
-                      <InfoWithIcon
-                        icon={<IconCalendarClock aria-hidden />}
-                        title={''}
-                      >
-                        {superEvent?.status === 'pending' ? (
-                          <SkeletonLoader />
-                        ) : (
-                          getDateRangeStr({
-                            start: eventStartTime || '',
-                            end: eventEndTime,
-                            locale,
-                            includeTime: true,
-                            timeAbbreviation:
-                              commonTranslation('timeAbbreviation'),
-                          })
-                        )}
-                      </InfoWithIcon>
-                    )}
-                  </div>
-                  {eventPriceText && (
-                    <div className={styles.price}>
-                      <InfoWithIcon
-                        icon={<IconTicket aria-hidden />}
-                        title={''}
-                      >
-                        {eventPriceText}
-                      </InfoWithIcon>
-                    </div>
-                  )}
-                  {offerInfoUrl && (
-                    <div className={styles.registrationButtonWrapper}>
-                      <Button
-                        theme={theme}
-                        variant={variant}
-                        className={buttonStyles.buttonCoatBlue}
-                        aria-label={buttonAriaLabelText}
-                        onClick={() => window.open(offerInfoUrl)}
-                        iconRight={<IconLinkExternal aria-hidden />}
-                      >
-                        {buttonText}
-                      </Button>
-                    </div>
-                  )}
+                  <LocationInfo event={event} />
+                  <TimeInfo event={event} superEvent={superEvent} />
+                  <EventPriceInfo event={event} />
+                  <OfferButton event={event} />
                 </div>
                 {showKeywords && (
                   <div className={styles.categoryWrapper}>

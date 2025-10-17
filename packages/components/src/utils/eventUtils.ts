df83fdb8b8c29439233b5a4f3a25e41e628d9977
Niko Lindroos <niko.j.makela@gmail.com>
2025-10-17 16:42:40 +0300

    feat: add new event hero button text for queueable event
    HH-449.
    
    Add a new text to buy / enrol button when the enrolment status for event
    is "queueable".

--- packages/components/src/utils/eventUtils.ts
+++ packages/components/src/utils/eventUtils.ts
@@ -4,6 +4,7 @@ import type { TFunction } from 'next-i18next';
 
 import {
   CITY_OF_HELSINKI_LINKED_EVENTS_ORGANIZATION_PREFIXES,
+  EnrolmentStatusLabel,
   EVENT_KEYWORD_BLACK_LIST,
   EVENT_LOCATIONS,
   EVENT_SOME_IMAGE,
@@ -17,6 +18,7 @@ import type {
   PlaceFieldsFragment,
 } from '../types';
 import { EventTypeId } from '../types';
+import { getEnrolmentStatus } from './getEventEnrolmentStatus';
 import getLocalizedString from './getLocalizedString';
 import getSecureImage from './getSecureImage';
 
@@ -132,6 +134,11 @@ export const getEventHeroButtonText = (
   prefix: string,
   t: TFunction
 ): string => {
+  const status = getEnrolmentStatus(event);
+
+  if (status === EnrolmentStatusLabel.queueable) {
+    return t(`hero.${prefix}QueueEnrol`);
+  }
   return t(
     `hero.${prefix}${
       event.typeId === EventTypeId.General && !isEventFree(event)

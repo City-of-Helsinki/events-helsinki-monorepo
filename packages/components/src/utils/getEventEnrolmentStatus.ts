4bac15cb52691fa7447b51f3ad3e08f1580d7c56
Niko Lindroos <niko.j.makela@gmail.com>
2025-10-17 15:27:22 +0300

    feat: add queueable status to enrolment status handling
    HH-449

--- packages/components/src/utils/getEventEnrolmentStatus.ts
+++ packages/components/src/utils/getEventEnrolmentStatus.ts
@@ -5,11 +5,23 @@ export function getEnrolmentStatus(event: EventFields): EnrolmentStatusLabel {
   const now = new Date();
 
   // NOTE: `event.registration` needs `includes: ['registration']` as a query param.
-  const { remainingAttendeeCapacity, enrolmentStartTime, enrolmentEndTime } =
-    event?.registration || { ...event, remainingAttendeeCapacity: undefined };
+  const {
+    remainingAttendeeCapacity,
+    enrolmentStartTime,
+    enrolmentEndTime,
+    waitingListCapacity,
+    remainingWaitingListCapacity,
+  } = event?.registration || {
+    ...event,
+    remainingAttendeeCapacity: undefined,
+    waitingListCapacity: undefined,
+    remainingWaitingListCapacity: undefined,
+  };
 
-  // TODO: Add EnrolmentStatusLabel.queueable when we can resolve it
   if (remainingAttendeeCapacity === 0) {
+    if (waitingListCapacity && remainingWaitingListCapacity) {
+      return EnrolmentStatusLabel.queueable;
+    }
     return EnrolmentStatusLabel.full;
   } else if (enrolmentStartTime && new Date(enrolmentStartTime) > now) {
     return EnrolmentStatusLabel.enrolmentNotStartedYet;

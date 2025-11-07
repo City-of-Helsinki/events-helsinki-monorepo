1ddd56d7068295de197532362dd69b44abd7b51b
Niko Lindroos <niko.j.makela@gmail.com>
2025-11-07 16:38:44 +0200

    WIP events cookie consent work

--- packages/components/src/components/eventsCookieConsent/EventsCookieConsent.tsx
+++ packages/components/src/components/eventsCookieConsent/EventsCookieConsent.tsx
@@ -13,6 +13,10 @@ import { useConsentTranslation } from '../../hooks';
 import useLocale from '../../hooks/useLocale';
 import useConsentSiteSettings from './useConsentSiteSettings';
 
+type CookieContextLanguage = NonNullable<
+  CookieConsentContextProps['options']
+>['language'];
+
 type Props = {
   appName: string;
   onConsentGiven?: () => void;
@@ -28,8 +32,7 @@ const EventsCookieConsent: React.FC<Props> = ({
   const locale = useLocale();
   const { asPath: pathname } = useRouter();
   const { t, i18n } = useConsentTranslation();
-  const [language, setLanguage] =
-    React.useState<ContentSource['currentLanguage']>(locale);
+  const [language, setLanguage] = React.useState<CookieContextLanguage>(locale);
   const { cookieDomain } = useCookieConfigurationContext();
   const { getAllConsents } = useCookies({ cookieDomain });
   const { pushInstruction } = useMatomo();
@@ -45,7 +48,7 @@ const EventsCookieConsent: React.FC<Props> = ({
   const onLanguageChange = React.useCallback(
     (newLang: string) => {
       if (allowLanguageSwitch) {
-        setLanguage(newLang as ContentSource['currentLanguage']);
+        setLanguage(newLang);
         i18n.changeLanguage(newLang);
       }
     },
@@ -65,152 +68,25 @@ const EventsCookieConsent: React.FC<Props> = ({
     }
   }, [getAllConsents, pushInstruction]);
 
-  const contentSource: ContentSource = React.useMemo(
-    () => ({
-      onAllConsentsGiven: () => {
-        setShowCookieConsentModal(false);
-        if (onConsentGiven) {
-          handleMatomoUpdate();
-          onConsentGiven();
-        }
-      },
-      currentLanguage: language as string as ContentSource['currentLanguage'],
-      requiredCookies: {
-        groups: [
-          {
-            commonGroup: 'essential',
-            cookies: [
-              {
-                id: 'wordpress',
-                name: 'wordpress_*, wp-settings-*',
-                hostName: 'api.hel.fi',
-                description: t('consent:cookies.wordpress'),
-                expiration: t('consent:expiration.session'),
-              },
-              {
-                id: 'linkedevents',
-                name: 'linkedevents-api-prod-csrftoken',
-                hostName: 'api.hel.fi',
-                description: t('consent:cookies.linkedevents'),
-                expiration: t('consent:expiration.year'),
-              },
-              {
-                id: 'i18next',
-                name: 'i18next',
-                hostName: 'api.hel.fi',
-                description: t('consent:cookies.i18next'),
-                expiration: t('consent:expiration.session'),
-              },
-            ],
-          },
-        ],
-      },
-      optionalCookies: {
-        groups: [
-          {
-            title: t('consent:groups.optionalServiceMap.title'),
-            text: t('consent:groups.optionalServiceMap.text'),
-            expandAriaLabel: t(
-              'consent:groups.optionalServiceMap.expandAriaLabel'
-            ),
-            checkboxAriaDescription: t(
-              'consent:groups.optionalServiceMap.checkboxAriaDescription'
-            ),
-            cookies: [
-              {
-                id: 'servicemap_analytics',
-                name: '_pk.fi*',
-                hostName: 'palvelukartta.hel.fi',
-                description: t('consent:cookies.servicemap_analytics'),
-                expiration: t('consent:expiration.days', { days: 393 }),
-              },
-              {
-                id: 'servicemap_session',
-                name: 'ADRUM',
-                hostName: 'palvelukartta.hel.fi',
-                description: t('consent:cookies.servicemap_session'),
-                expiration: t('consent:expiration.session'),
-              },
-              {
-                id: 'servicemap_ga',
-                name: '_ga*',
-                hostName: 'palvelukartta.hel.fi',
-                description: t('consent:cookies.servicemap_ga'),
-                expiration: t('consent:expiration.session'),
-              },
-            ],
-          },
-          {
-            title: t('consent:groups.optionalMatomo.title'),
-            text: t('consent:groups.optionalMatomo.text'),
-            expandAriaLabel: t('consent:groups.optionalMatomo.expandAriaLabel'),
-            checkboxAriaDescription: t(
-              'consent:groups.optionalMatomo.checkboxAriaDescription'
-            ),
-            cookies: [
-              {
-                id: 'matomo',
-                name: '_pk*',
-                hostName: 'digia.fi',
-                description: t('consent:cookies.matomo'),
-                expiration: t('consent:expiration.days', { days: 393 }),
-              },
-            ],
-          },
-          {
-            title: t('consent:groups.optionalAskem.title'),
-            text: t('consent:groups.optionalAskem.text'),
-            expandAriaLabel: t('consent:groups.optionalAskem.expandAriaLabel'),
-            checkboxAriaDescription: t(
-              'consent:groups.optionalAskem.checkboxAriaDescription'
-            ),
-            cookies: [
-              {
-                id: 'askemBid',
-                name: 'rnsbid',
-                hostName: 'reactandshare.com',
-                description: t('consent:cookies.askem'),
-                expiration: '-',
-              },
-              {
-                id: 'askemBidTs',
-                name: 'rnsbid_ts',
-                hostName: 'reactandshare.com',
-                description: t('consent:cookies.askem'),
-                expiration: '-',
-              },
-              {
-                id: 'askemReaction',
-                name: 'rns_reaction_*',
-                hostName: 'reactandshare.com',
-                description: t('consent:cookies.askem'),
-                expiration: '-',
-              },
-            ],
-          },
-        ],
-      },
-      language: {
-        current: language,
-        onLanguageChange,
-      },
-      focusTargetSelector: MAIN_CONTENT_ID,
-    }),
-    [language, t, onLanguageChange, onConsentGiven, handleMatomoUpdate]
-  );
+  const siteSettings = useConsentSiteSettings();
 
   const onChange = () => {
     console.log('TODO: implement onChange handler to cookie consent');
   };
 
-  const siteSettings = useConsentSiteSettings();
-
+  const onAllConsentsGiven = () => {
+    setShowCookieConsentModal(false);
+    if (onConsentGiven) {
+      handleMatomoUpdate();
+      onConsentGiven();
+    }
+  };
   return (
     <CookieConsentContextProvider
       onChange={onChange}
       // focusing the logo link, because the tab component loses focus on re-render.
       options={{ language, focusTargetSelector: '#actionbar > a' }}
-      siteSettings={{ ...siteSettings, remove: false, monitorInterval: 0 }}
+      siteSettings={siteSettings}
     >
       {isModal && showCookieConsentModal && <CookieBanner />}
       {!isModal && <CookieSettingsPage />}

f402e02993fa499b923047a74b03caa4b67ee388
Niko Lindroos <niko.j.makela@gmail.com>
2025-11-08 21:08:53 +0200

    WIP fix matomo wrapepr

--- packages/components/src/components/matomoWrapper/MatomoWrapper.tsx
+++ packages/components/src/components/matomoWrapper/MatomoWrapper.tsx
@@ -1,9 +1,8 @@
 import useMatomo from '@jonkoops/matomo-tracker-react/lib/useMatomo.js';
-import { useCookies } from 'hds-react';
+import { useGroupConsent } from 'hds-react';
 import { useRouter } from 'next/router';
 import type { ReactNode } from 'react';
 import React, { useEffect } from 'react';
-import { useCookieConfigurationContext } from '../../cookieConfigurationProvider';
 
 interface Props {
   children?: ReactNode | undefined;
@@ -12,17 +11,11 @@ interface Props {
 const MatomoWrapper: React.FC<Props> = ({ children }) => {
   const { trackPageView, pushInstruction } = useMatomo();
   const { asPath: pathname } = useRouter();
-  const { cookieDomain } = useCookieConfigurationContext();
-  const { getAllConsents } = useCookies({ cookieDomain });
+  const isConsentGiven = useGroupConsent('matomo');
 
   // Track page changes when pathnname changes
   useEffect(() => {
-    const getConsentStatus = (cookieId: string) => {
-      const consents = getAllConsents();
-      return consents[cookieId];
-    };
-
-    if (getConsentStatus('matomo')) {
+    if (isConsentGiven) {
       pushInstruction('setCookieConsentGiven');
     } else {
       pushInstruction('forgetCookieConsentGiven');
@@ -32,7 +25,7 @@ const MatomoWrapper: React.FC<Props> = ({ children }) => {
     trackPageView({
       href: window.location.href,
     });
-  }, [getAllConsents, pathname, pushInstruction, trackPageView]);
+  }, [isConsentGiven, pathname, pushInstruction, trackPageView]);
 
   return <>{children}</>;
 };

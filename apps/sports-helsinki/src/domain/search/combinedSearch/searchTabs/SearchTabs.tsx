2372107f881e084382c15eb17cb81e81715eec74
Niko Lindroos <niko.j.makela@gmail.com>
2025-11-21 16:54:47 +0200

    refactor: add more detailed comments to visibleOptions of Select
    HCRC-178

--- apps/sports-helsinki/src/domain/search/combinedSearch/searchTabs/SearchTabs.tsx
+++ apps/sports-helsinki/src/domain/search/combinedSearch/searchTabs/SearchTabs.tsx
@@ -121,7 +121,9 @@ function SearchTabListMobile({ data }: SearchTabListMobileProps) {
         value={activeTab}
         onChange={handleSearchTabChange}
         options={options}
-        visibleOptions={5.97} // use decimal to make scrollable content visible
+        // Show next option partially to indicate more is available.
+        // See more: (https://hds.hel.fi/components/select/code/#component-properties)
+        visibleOptions={5.97}
       />
     </div>
   );
